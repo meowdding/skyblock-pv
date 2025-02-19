b72e3f2ade693166ea7ef0b8fe23ce6fb2ee2788
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-19 14:09:46 +0100

    not scrolling mobkills screen
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/api/data/SkyblockProfile.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/data/SkyblockProfile.kt
@@ -5,6 +5,7 @@ import net.minecraft.Util
 import tech.thatgravyboat.skyblockapi.api.remote.SkyBlockItems
 import tech.thatgravyboat.skyblockpv.data.CollectionCategory
 import tech.thatgravyboat.skyblockpv.data.CollectionItem
+import tech.thatgravyboat.skyblockpv.data.MobData
 import tech.thatgravyboat.skyblockpv.utils.*
 import java.util.*
 
@@ -13,6 +14,7 @@ data class SkyblockProfile(
     val id: ProfileId,
 
     val collections: List<CollectionItem>,
+    val mobData: List<MobData>,
 ) {
     companion object {
 
@@ -28,6 +30,20 @@ data class SkyblockProfile(
 
                 collections = member["collection"].asMap { string, element -> string to element.asLong(0) }.mapNotNull { (id, amount) ->
                     CollectionCategory.getCategoryByItemName(id)?.let { CollectionItem(it, id, SkyBlockItems.getItemById(id), amount) }
+                },
+
+                mobData = run {
+                    val playerStats = member.getAsJsonObject("player_stats") ?: return@run emptyList()
+                    val deaths = playerStats["deaths"].asMap { id, amount -> id to amount.asLong(0) }
+                    val kills = playerStats["kills"].asMap { id, amount -> id to amount.asLong(0) }
+
+                    (deaths.keys + kills.keys).map { id ->
+                        MobData(
+                            mobId = id,
+                            kills = kills[id] ?: 0,
+                            deaths = deaths[id] ?: 0,
+                        )
+                    }
                 },
             )
         }
---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/data/MobData.kt
@@ -0,0 +1,7 @@
+package tech.thatgravyboat.skyblockpv.data
+
+data class MobData(
+    val mobId: String,
+    val kills: Long,
+    val deaths: Long,
+)
