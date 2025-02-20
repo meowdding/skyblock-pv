7118674f1286b743f82f62d306334403884f3f98
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-20 15:09:39 +0100

    refactor(collection screen): add table display, use table instead of 2 scrollables

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/CollectionScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/CollectionScreen.kt
@@ -1,7 +1,6 @@
 package tech.thatgravyboat.skyblockpv.screens
 
 import com.teamresourceful.resourcefullib.client.screens.BaseCursorScreen
-import earth.terrarium.olympus.client.components.base.BaseWidget
 import earth.terrarium.olympus.client.components.base.ListWidget
 import earth.terrarium.olympus.client.ui.UIConstants
 import kotlinx.coroutines.CoroutineScope
@@ -9,7 +8,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.layouts.FrameLayout
-import net.minecraft.client.gui.layouts.LinearLayout
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockapi.utils.text.CommonText
@@ -18,19 +16,21 @@ import tech.thatgravyboat.skyblockpv.api.ProfileAPI
 import tech.thatgravyboat.skyblockpv.data.CollectionCategory
 import tech.thatgravyboat.skyblockpv.data.CollectionItem
 import tech.thatgravyboat.skyblockpv.utils.displays.Alignment
+import tech.thatgravyboat.skyblockpv.utils.displays.Display
 import tech.thatgravyboat.skyblockpv.utils.displays.Displays
+import tech.thatgravyboat.skyblockpv.utils.displays.asTable
 import tech.thatgravyboat.skyblockpv.utils.displays.asWidget
+import tech.thatgravyboat.skyblockpv.utils.displays.centerIn
 import java.util.*
 
 private const val ASPECT_RATIO = 9.0 / 16.0
 
 object CollectionScreen : BaseCursorScreen(CommonText.EMPTY) {
-    private var currentCategory = CollectionCategory.FARMING
+    private var currentCategory = CollectionCategory.MINING
 
     override fun init() {
         val width = (this.width * 0.6).toInt()
         val height = (width * ASPECT_RATIO).toInt()
-        val columnWidth = width / 2 - 20
         val columnHeight = height - 20
 
         val bg = Displays.background(UIConstants.BUTTON.enabled, width, height).asWidget()
@@ -42,41 +42,38 @@ object CollectionScreen : BaseCursorScreen(CommonText.EMPTY) {
 
             val profile = ProfileAPI.getProfiles(uuid).find { it.selected } ?: return@launch
 
-            val columnLeft = ListWidget(columnWidth, columnHeight)
-            val columnRight = ListWidget(columnWidth, columnHeight)
+            val scrollable = ListWidget(width, columnHeight)
 
             val filteredCollections = profile.collections.filter { it.category == currentCategory }
 
-            filteredCollections.chunked(2).forEach { chunk ->
-                chunk.getOrNull(0)?.let { columnLeft.add(getElement(it)) }
-                chunk.getOrNull(1)?.let { columnRight.add(getElement(it)) }
-            }
+            val table = buildList {
+                filteredCollections.chunked(2).forEach { chunk ->
+                    val row = buildList {
+                        chunk.getOrNull(0)?.let { add(getElement(it)) }
+                        chunk.getOrNull(1)?.let { add(getElement(it)) }
+                    }
+                    add(row)
+                }
+            }.asTable().centerIn(width, -1).asWidget()
 
-            val row = LinearLayout.horizontal().spacing(5)
-            row.addChild(columnLeft)
-            row.addChild(columnRight)
-
-            row.arrangeElements()
+            scrollable.add(table)
 
             FrameLayout.centerInRectangle(bg, 0, 0, screen.width, screen.height)
-            FrameLayout.centerInRectangle(row, bg.x, bg.y, bg.width, bg.height)
+            FrameLayout.centerInRectangle(scrollable, bg.x, bg.y, bg.width, bg.height)
 
             bg.visitWidgets(screen::addRenderableOnly)
-            row.visitWidgets(screen::addRenderableWidget)
+            scrollable.visitWidgets(screen::addRenderableWidget)
         }
     }
 
-    private fun getElement(col: CollectionItem): BaseWidget {
-        val display = Displays.fixed(
-            300, 16,
-            Displays.row(
-                Displays.item(col.itemStack ?: ItemStack.EMPTY),
-                Displays.text(Text.join(col.itemStack?.hoverName ?: col.itemId, ": ${col.amount.toFormattedString()}")),
-                spacing = 5,
-                alignment = Alignment.CENTER,
-            )
+    private fun getElement(col: CollectionItem): Display {
+        val display = Displays.row(
+            Displays.item(col.itemStack ?: ItemStack.EMPTY),
+            Displays.text(Text.join(col.itemStack?.hoverName ?: col.itemId, ": ${col.amount.toFormattedString()}")),
+            spacing = 5,
+            alignment = Alignment.CENTER,
         )
-        return display.asWidget()
+        return display
     }
 
     override fun renderBackground(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
