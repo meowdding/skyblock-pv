cb0237b5285ca99bced730783d9001ef96867098
Mayaqq <73876072+MayaqqDev@users.noreply.github.com>
2025-02-20 20:45:56 +0100

    Base Screen Widget, Main Widget and a bases for tabs (#2)
    * Tried to work on the cool pv command with a suggestion provider and everything
    
    * fix: 3rd times the charm
    
    * Base Screen Widget
    
    * some sort of basis for the different tabs

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/MainScreen.kt
@@ -0,0 +1,11 @@
+package tech.thatgravyboat.skyblockpv.screens.tabs
+
+import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
+import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
+import java.util.UUID
+
+class MainScreen(uuid: UUID) : BasePvScreen("MAIN", uuid) {
+    override suspend fun create(width: Int, height: Int, bg: DisplayWidget) {
+
+    }
+}
