d64962968ff29b07acca3f91a8785248c77f03e5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-20 21:36:23 +0100

    refactor: create uiWidth, uiHeight vals inside basescreen
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/MainScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/MainScreen.kt
@@ -2,10 +2,10 @@ package tech.thatgravyboat.skyblockpv.screens.tabs
 
 import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
 import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
-import java.util.UUID
+import java.util.*
 
 class MainScreen(uuid: UUID) : BasePvScreen("MAIN", uuid) {
-    override suspend fun create(width: Int, height: Int, bg: DisplayWidget) {
+    override suspend fun create(bg: DisplayWidget) {
 
     }
 }
