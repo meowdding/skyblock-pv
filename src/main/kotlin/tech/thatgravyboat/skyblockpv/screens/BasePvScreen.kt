c65054495abfd514a39b244cd598533965b42fb2
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-21 15:32:54 +0100

    chore: replace CoroutineScopes with runBlocking
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/BasePvScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/BasePvScreen.kt
@@ -5,9 +5,7 @@ import earth.terrarium.olympus.client.components.buttons.Button
 import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
 import earth.terrarium.olympus.client.components.string.TextWidget
 import earth.terrarium.olympus.client.ui.UIConstants
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.layouts.FrameLayout
 import net.minecraft.client.gui.layouts.LinearLayout
@@ -32,7 +30,7 @@ abstract class BasePvScreen(val name: String, val uuid: UUID) : BaseCursorScreen
         val bg = Displays.background(UIConstants.BUTTON.enabled, uiWidth, uiHeight).asWidget()
         val loading = TextWidget(Component.literal("Loading..."))
 
-        CoroutineScope(Dispatchers.IO).launch {
+        runBlocking {
             val screen = this@BasePvScreen
 
             FrameLayout.centerInRectangle(bg, 0, 0, screen.width, screen.height)
