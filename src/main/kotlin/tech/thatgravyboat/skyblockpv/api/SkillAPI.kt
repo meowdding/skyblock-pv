3985e5ea40716a769307dc5736883b0cb438431f
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-21 18:02:23 +0100

    refactor(SkillAPI): use separate list for SkillXP
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/api/SkillAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/SkillAPI.kt
@@ -3,6 +3,7 @@ package tech.thatgravyboat.skyblockpv.api
 import com.google.gson.JsonObject
 import kotlinx.coroutines.runBlocking
 import tech.thatgravyboat.skyblockapi.utils.http.Http
+import tech.thatgravyboat.skyblockpv.data.SkillData
 import tech.thatgravyboat.skyblockpv.utils.asInt
 import tech.thatgravyboat.skyblockpv.utils.asLong
 import tech.thatgravyboat.skyblockpv.utils.asString
@@ -12,41 +13,27 @@ private const val API_URL = "https://api.hypixel.net/v2/resources/skyblock/skill
 object SkillAPI {
     var skillData: Map<String, SkillData> = emptyMap()
         private set
+    var skillLevels: Map<Int, Long> = emptyMap()
+        private set
 
     init {
         runBlocking {
-            skillData = get()?.getAsJsonObject("skills")?.entrySet()?.associate { (key, value) ->
+            val skills = get()?.getAsJsonObject("skills") ?: return@runBlocking
+            skillData = skills.entrySet().associate { (key, value) ->
                 key to value.asJsonObject.toSkillData()
-            } ?: emptyMap()
-            println(skillData)
+            }
+            skillLevels = skills.entrySet().first().value.asJsonObject.getAsJsonArray("levels").associate {
+                it.asJsonObject.let {
+                    it["level"].asInt(0) to it["totalExpRequired"].asLong(0)
+                }
+            }
         }
     }
 
     private fun JsonObject.toSkillData() = SkillData(
         name = this["name"].asString(""),
-        description = this["description"].asString(""),
         maxLevel = this["maxLevel"].asInt(0),
-        levels = getAsJsonArray("levels").mapNotNull { it.asJsonObject.toSkillLevel() },
-    )
-
-    private fun JsonObject.toSkillLevel() = SkillLevel(
-        level = this["level"].asInt(0),
-        totalExpRequired = this["totalExpRequired"].asLong(0),
-        //unlocks = getAsJsonArray("unlocks").mapNotNull { it.asString },
     )
 
     private suspend fun get(): JsonObject? = Http.getResult<JsonObject>(url = API_URL).getOrNull()
 }
-
-data class SkillData(
-    val name: String,
-    val description: String,
-    val maxLevel: Int,
-    val levels: List<SkillLevel>,
-)
-
-data class SkillLevel(
-    val level: Int,
-    val totalExpRequired: Long,
-    //val unlocks: List<String>,
-)
