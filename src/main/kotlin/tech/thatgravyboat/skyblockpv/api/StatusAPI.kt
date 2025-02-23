c5c5b2cd247f89d8f1e295e479d51e070f8726b7
Mayaqq <73876072+MayaqqDev@users.noreply.github.com>
2025-02-23 22:42:34 +0100

    StatusAPI (#10)
    * StatusAPI
    
    * remove useless toString
    
    * Status Button

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/StatusAPI.kt
@@ -0,0 +1,18 @@
+package tech.thatgravyboat.skyblockpv.api
+
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.Text.send
+import tech.thatgravyboat.skyblockpv.api.data.PlayerStatus
+import java.util.*
+
+private const val PATH = "v2/status"
+
+object StatusAPI {
+    suspend fun getStatus(uuid: UUID): PlayerStatus? {
+        val result = HypixelAPI.get(PATH, mapOf("uuid" to uuid.toString())) ?: run {
+            Text.of("Something went wrong :3").send()
+            return null
+        }
+        return PlayerStatus.fromJson(result)
+    }
+}
---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/elements/ExtraConstants.kt
@@ -0,0 +1,9 @@
+package tech.thatgravyboat.skyblockpv.screens.elements
+
+import earth.terrarium.olympus.client.ui.UIConstants
+import net.minecraft.client.gui.components.WidgetSprites
+import tech.thatgravyboat.skyblockpv.SkyBlockPv.id
+
+object ExtraConstants {
+    val BUTTON_DARK = WidgetSprites(UIConstants.id("buttons/dark/normal"), id("buttons/dark/disabled"), UIConstants.id("buttons/dark/hovered"))
+}
