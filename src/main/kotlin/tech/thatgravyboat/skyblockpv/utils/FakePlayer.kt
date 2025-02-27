dd0fe6eee984c65dee05eda208ab66e090451f3c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-27 12:50:31 +0100

    feat: FakePlayer now has a custom name placeholder until im bothered to actually impl

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/FakePlayer.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/FakePlayer.kt
@@ -3,9 +3,13 @@ package tech.thatgravyboat.skyblockpv.utils
 import com.mojang.authlib.GameProfile
 import net.minecraft.client.Minecraft
 import net.minecraft.client.player.RemotePlayer
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.world.entity.player.PlayerModelPart
 import net.minecraft.world.scores.PlayerTeam
+import net.minecraft.world.scores.Scoreboard
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.Text
 
 class FakePlayer(gameProfile: GameProfile) : RemotePlayer(McClient.self.level, gameProfile) {
     override fun isSpectator() = false
@@ -13,9 +17,9 @@ class FakePlayer(gameProfile: GameProfile) : RemotePlayer(McClient.self.level, g
 
     override fun getSkin() = Minecraft.getInstance().skinManager.lookupInsecure(gameProfile).get()
 
-    override fun getTeam() = object : PlayerTeam(null, "display") {
-        override fun getNameTagVisibility() = Visibility.NEVER
-    }
+    override fun getScoreboard() = object : Scoreboard(){}
 
-    override fun isModelPartShown(part: PlayerModelPart) = true
+    override fun getDisplayName(): Component? = Text.join(gameProfile.name, " is gay, ew")
+
+    override fun isModelPartShown(part: PlayerModelPart) = part != PlayerModelPart.CAPE
 }
