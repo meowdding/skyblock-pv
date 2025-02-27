c604bad40dcbcd004bbaa0de03c71790dd63ebfa
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-27 09:07:14 +0100

    refactor: made widget with bg method public
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/DungeonScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/DungeonScreen.kt
@@ -2,20 +2,25 @@ package tech.thatgravyboat.skyblockpv.screens.tabs
 
 import com.mojang.authlib.GameProfile
 import net.minecraft.client.gui.layouts.FrameLayout
+import net.minecraft.client.gui.layouts.LayoutElement
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
 import tech.thatgravyboat.skyblockpv.data.DungeonData
 import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
-import tech.thatgravyboat.skyblockpv.utils.displays.Display
+import tech.thatgravyboat.skyblockpv.utils.Utils
+import tech.thatgravyboat.skyblockpv.utils.Utils.round
 import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
 import tech.thatgravyboat.skyblockpv.utils.displays.Displays
 import tech.thatgravyboat.skyblockpv.utils.displays.asTable
+import tech.thatgravyboat.skyblockpv.utils.displays.asWidget
 
 class DungeonScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePvScreen("DUNGEON", gameProfile, profile) {
     override fun create(bg: DisplayWidget) {
         val dungeonData = profile?.dungeonData!!
         val row = LayoutBuild.horizontal(5) {
-            display(createRunsDisplay(dungeonData))
+            widget(createInfoBoxDisplay(dungeonData))
+            widget(createRunsDisplay(dungeonData))
         }
 
         FrameLayout.centerInRectangle(row, bg.x, bg.y, bg.width, bg.height)
@@ -23,7 +28,28 @@ class DungeonScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) 
         row.visitWidgets(this::addRenderableWidget)
     }
 
-    private fun createRunsDisplay(dungeonData: DungeonData): Display {
+    private fun countRuns(completions: Map<String, Long>?) = completions?.filterKeys { it != "total" }?.values?.sum() ?: 0
+
+    private fun createInfoBoxDisplay(dungeonData: DungeonData) = LayoutBuild.vertical {
+        val width = 200
+        val catacombsCompl = dungeonData.dungeonTypes["catacombs"]?.tierCompletions
+        val masterModeCompl = dungeonData.dungeonTypes["master_catacombs"]?.tierCompletions
+
+        widget(Utils.getTitleWidget("Dungeon Info", width))
+        widget(
+            Utils.getMainContentWidget(
+                LayoutBuild.vertical {
+                    string("Class Average: TODO")
+                    string("Secrets: ${dungeonData.secrets.toFormattedString()}")
+                    string("Secrets/Run: ${(dungeonData.secrets / (countRuns(catacombsCompl) + countRuns(masterModeCompl))).round()}")
+                },
+                width,
+            ),
+        )
+    }
+
+    private fun createRunsDisplay(dungeonData: DungeonData): LayoutElement {
+        val width = 200
         val catacombs = dungeonData.dungeonTypes["catacombs"]
         val masterMode = dungeonData.dungeonTypes["master_catacombs"]
         val catacombsComp = catacombs?.tierCompletions
@@ -33,9 +59,7 @@ class DungeonScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) 
             add(listOf(name, catacombsComp?.get(floor)?.toString() ?: "0", masterComp?.get(floor)?.toString() ?: "0"))
         }
 
-        fun countRuns(completions: Map<String, Long>?) = completions?.filterKeys { it != "total" }?.values?.sum() ?: 0
-
-        return buildList {
+        val table = buildList {
             add(listOf("", "Catacombs", "Master"))
             getRow("Bonzo", "1")
             getRow("Scarf", "2")
@@ -45,7 +69,12 @@ class DungeonScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) 
             getRow("Sadan", "6")
             getRow("Necron", "7")
             add(listOf("Total", countRuns(catacombsComp).toString(), countRuns(masterComp).toString()))
-        }.map { it.map { Displays.text(it) } }.asTable(10)
+        }.map { it.map { Displays.text(it, color = { 0x555555u }, shadow = false) } }.asTable(10).asWidget()
+
+        return LayoutBuild.vertical {
+            widget(Utils.getTitleWidget("Dungeon Runs", width))
+            widget(Utils.getMainContentWidget(table, width))
+        }
     }
 
 }
