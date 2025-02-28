83e48d83835c292dc654a2a577e837031f73e04d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-02-28 11:23:50 +0100

    feat: skyblock lvl in fakeplayer
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/api/HypixelAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/HypixelAPI.kt
@@ -5,7 +5,7 @@ import tech.thatgravyboat.skyblockapi.utils.http.Http
 
 // TODO switch to some other api :3
 private const val API_URL = "https://api.hypixel.net/%s"
-private const val API_KEY = "aa06b3ef-1d8d-4b44-91dc-069baae744db"
+private const val API_KEY = "8988e0a9-77ab-445e-a4ff-f4242b6f0670"
 
 object HypixelAPI {
 
--- src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/FakePlayer.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/FakePlayer.kt
@@ -4,22 +4,19 @@ import com.mojang.authlib.GameProfile
 import net.minecraft.client.Minecraft
 import net.minecraft.client.player.RemotePlayer
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.MutableComponent
 import net.minecraft.world.entity.player.PlayerModelPart
-import net.minecraft.world.scores.PlayerTeam
 import net.minecraft.world.scores.Scoreboard
 import tech.thatgravyboat.skyblockapi.helpers.McClient
-import tech.thatgravyboat.skyblockapi.utils.text.Text
 
-class FakePlayer(gameProfile: GameProfile) : RemotePlayer(McClient.self.level, gameProfile) {
+class FakePlayer(gameProfile: GameProfile, val customDisplayName: Component) : RemotePlayer(McClient.self.level, gameProfile) {
     override fun isSpectator() = false
     override fun isCreative() = false
 
     override fun getSkin() = Minecraft.getInstance().skinManager.lookupInsecure(gameProfile).get()
 
-    override fun getScoreboard() = object : Scoreboard(){}
+    override fun getScoreboard() = object : Scoreboard() {}
 
-    override fun getDisplayName(): Component? = Text.join(gameProfile.name, " is gay, ew")
+    override fun getDisplayName(): Component? = customDisplayName
 
     override fun isModelPartShown(part: PlayerModelPart) = part != PlayerModelPart.CAPE
 }
