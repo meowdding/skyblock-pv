4d8aefe1c7318c813b85d25710ed19daaa3396a9
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-01 17:24:15 +0100

    feat: Miningcore data
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/data/MiningData.kt
@@ -0,0 +1,20 @@
+package tech.thatgravyboat.skyblockpv.data
+
+data class MiningCore(
+    val nodes: Map<String, Int>,
+    val crystals: Map<String, Crystal>,
+    val experience: Long,
+    val powderMithril: Int,
+    val powderSpentMithril: Int,
+    val powderGemstone: Int,
+    val powderSpentGemstone: Int,
+    val powderGlacite: Int,
+    val powderSpentGlacite: Int,
+
+    )
+
+data class Crystal(
+    val state: String,
+    val totalPlaced: Int,
+    val totalFound: Int,
+)
