e22b9ed9f346db898d1cec7cfd93b36734714872
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-06 09:51:02 +0100

    refactor: use getOrPut in ProfileAPI
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/api/ProfileAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/ProfileAPI.kt
@@ -10,32 +10,22 @@ private const val CACHE_TIME = 10 * 60 * 1000 // 10 minutes
 
 object ProfileAPI {
 
-    private val cache: MutableMap<String, CacheEntry> = mutableMapOf()
+    private val cache: MutableMap<UUID, CacheEntry> = mutableMapOf()
 
-    private fun getCachedProfiles(uuid: UUID): List<SkyBlockProfile>? {
-        val entry = cache[uuid.toString()] ?: return null
-        return if (System.currentTimeMillis() - entry.timestamp < CACHE_TIME) {
-            entry.profiles
-        } else {
-            cache.remove(uuid.toString())
-            null
-        }
-    }
-
-    suspend fun getProfiles(uuid: UUID): List<SkyBlockProfile> {
-        val cached = getCachedProfiles(uuid)
-        if (cached != null) return cached
-
+    suspend fun getProfiles(uuid: UUID): List<SkyBlockProfile> = cache.getOrPut(uuid) {
         val result = HypixelAPI.get(PATH, mapOf("uuid" to uuid.toString())) ?: run {
             Text.of("Something went wrong :3").send()
             return emptyList()
         }
 
-        val profiles = result.getAsJsonArray("profiles").mapNotNull { SkyBlockProfile.fromJson(it.asJsonObject, uuid) }
+        val profiles = result.getAsJsonArray("profiles").mapNotNull {
+            SkyBlockProfile.fromJson(it.asJsonObject, uuid)
+        }
 
-        cache[uuid.toString()] = CacheEntry(profiles, System.currentTimeMillis())
-
-        return profiles
+        CacheEntry(profiles, System.currentTimeMillis())
+    }.takeIf { System.currentTimeMillis() - it.timestamp < CACHE_TIME }?.profiles ?: run {
+        cache.remove(uuid)
+        getProfiles(uuid)
     }
 }
 
