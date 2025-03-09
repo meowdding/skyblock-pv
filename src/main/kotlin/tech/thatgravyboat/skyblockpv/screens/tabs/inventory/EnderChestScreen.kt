9e4e6da6bcbe3881c4f524dd723fe26529f270ac
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-09 23:11:04 +0100

    feat(inventory): enderchest icons
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
@@ -2,12 +2,15 @@ package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
 import earth.terrarium.olympus.client.components.buttons.Button
+import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
 import net.minecraft.client.gui.layouts.LinearLayout
+import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
 import tech.thatgravyboat.skyblockpv.screens.elements.ExtraConstants
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
 import tech.thatgravyboat.skyblockpv.utils.Utils.centerHorizontally
 import tech.thatgravyboat.skyblockpv.utils.components.CarouselWidget
+import tech.thatgravyboat.skyblockpv.utils.displays.Displays
 
 class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BaseInventoryScreen(gameProfile, profile) {
     var page = 0
@@ -17,9 +20,16 @@ class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = nul
         val buttonContainer = LinearLayout.horizontal().spacing(1)
 
         repeat(inventory.enderChestPages!!.size) { index ->
+            val enderChestItem = Items.ENDER_CHEST.defaultInstance
+            enderChestItem.count = index + 1
+            val itemDisplay = Displays.item(enderChestItem, showStackSize = true)
+
             val button = Button()
                 .withSize(20, 20)
                 .withTexture(ExtraConstants.BUTTON_DARK)
+                .withRenderer(
+                    WidgetRenderers.center(16, 16) { gr, ctx, _ -> itemDisplay.render(gr, ctx.x, ctx.y) },
+                )
                 .withCallback {
                     page = index
                     this@EnderChestScreen.rebuildWidgets()
