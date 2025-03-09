9ce985df5a67d02aeec5ff46f5d791967def40c7
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-09 20:06:48 +0100

    refactor(inventory): create basescreen
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
@@ -2,36 +2,17 @@ package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
 import earth.terrarium.olympus.client.components.buttons.Button
-import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
-import net.minecraft.client.gui.layouts.FrameLayout
 import net.minecraft.client.gui.layouts.LinearLayout
-import net.minecraft.world.item.ItemStack
-import tech.thatgravyboat.skyblockapi.helpers.McClient
-import tech.thatgravyboat.skyblockpv.SkyBlockPv
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
-import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
 import tech.thatgravyboat.skyblockpv.screens.elements.ExtraConstants
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
 import tech.thatgravyboat.skyblockpv.utils.Utils.centerHorizontally
 import tech.thatgravyboat.skyblockpv.utils.components.CarouselWidget
-import tech.thatgravyboat.skyblockpv.utils.displays.Display
-import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
-import tech.thatgravyboat.skyblockpv.utils.displays.Displays
-import tech.thatgravyboat.skyblockpv.utils.displays.asTable
 
-class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePvScreen("INVENTORY", gameProfile, profile) {
-    override fun create(bg: DisplayWidget) {
-        profile ?: return
-        val inv = createSecondaryInventory()
-        FrameLayout.centerInRectangle(inv, bg.x, bg.y, uiWidth, uiHeight)
-        inv.visitWidgets(this::addRenderableWidget)
-
-        addCategories(bg)
-    }
-
+class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BaseInventoryScreen(gameProfile, profile) {
     var page = 0
 
-    private fun createSecondaryInventory() = LayoutBuild.vertical {
+    override fun createInventoryWidget() = LayoutBuild.vertical {
         val inventory = profile?.inventory ?: return@vertical
         val buttonContainer = LinearLayout.horizontal().spacing(1)
 
@@ -52,51 +33,10 @@ class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = nul
 
         widget(
             CarouselWidget(
-                inventory.enderChestPages.map { createPagedInventory(it.items.inventory) },
+                inventory.enderChestPages.map { createInventory(it.items.inventory) },
                 page,
                 246,
             ).centerHorizontally(uiWidth),
         )
-
     }
-
-
-    private fun createPagedInventory(items: List<ItemStack>): Display {
-        val itemDisplays = items.chunked(9).map { chunk ->
-            chunk.map { item ->
-                Displays.padding(2, Displays.item(item, showTooltip = true, showStackSize = true))
-            }
-        }
-        return Displays.background(
-            SkyBlockPv.id("inventory/inventory-9x${itemDisplays.size}"),
-            Displays.padding(2, itemDisplays.asTable()),
-        )
-    }
-
-    private fun addCategories(bg: DisplayWidget) {
-        val categories = InventoryCategory.entries
-        val buttonRow = LinearLayout.horizontal().spacing(2)
-        categories.forEach { category ->
-            val button = Button()
-            button.setSize(20, 22)
-            if (category.isSelected()) {
-                button.withTexture(ExtraConstants.TAB_TOP_SELECTED)
-            } else {
-                button.withCallback { McClient.tell { McClient.setScreen(category.create(gameProfile, profile)) } }
-                button.withTexture(ExtraConstants.TAB_TOP)
-            }
-            button.withRenderer(
-                WidgetRenderers.padded(
-                    4, 0, 0, 0,
-                    WidgetRenderers.center(16, 16) { gr, ctx, _ -> gr.renderItem(category.icon, ctx.x, ctx.y) },
-                ),
-            )
-            buttonRow.addChild(button)
-        }
-        buttonRow.arrangeElements()
-        buttonRow.setPosition(bg.x + 20, bg.y - buttonRow.height)
-        buttonRow.visitWidgets(this::addRenderableWidget)
-    }
-
-    private fun List<ItemStack>?.orEmpty(size: Int) = this ?: List(size) { ItemStack.EMPTY }
 }
--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/InventoryScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/InventoryScreen.kt
@@ -1,31 +1,14 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
-import earth.terrarium.olympus.client.components.buttons.Button
-import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
-import net.minecraft.client.gui.layouts.FrameLayout
-import net.minecraft.client.gui.layouts.LinearLayout
-import net.minecraft.world.item.ItemStack
-import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockpv.SkyBlockPv
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
-import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
-import tech.thatgravyboat.skyblockpv.screens.elements.ExtraConstants
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
 import tech.thatgravyboat.skyblockpv.utils.Utils.center
 import tech.thatgravyboat.skyblockpv.utils.displays.*
 
-class InventoryScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePvScreen("INVENTORY", gameProfile, profile) {
-    override fun create(bg: DisplayWidget) {
-        profile ?: return
-        val inv = createMainInventoryRow()
-        FrameLayout.centerInRectangle(inv, bg.x, bg.y, uiWidth, uiHeight)
-        inv.visitWidgets(this::addRenderableWidget)
-
-        addCategories(bg)
-    }
-
-    private fun createMainInventoryRow() = LayoutBuild.horizontal {
+class InventoryScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BaseInventoryScreen(gameProfile, profile) {
+    override fun createInventoryWidget() = LayoutBuild.horizontal {
         val inventory = profile?.inventory ?: return@horizontal
         val armor = inventory.armorItems?.inventory.orEmpty(4)
         val equipment = inventory.equipmentItems?.inventory.orEmpty(4)
@@ -44,46 +27,8 @@ class InventoryScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null
         )
 
         spacer(width = 10)
-        widget(createInventory(inventory.inventoryItems?.inventory.orEmpty(36)).center(-1, height))
+        val inventoryItems = inventory.inventoryItems?.inventory.orEmpty(36).chunked(9)
+        val reorderedItems = (inventoryItems.drop(1) + inventoryItems.take(1)).flatten()
+        widget(createInventory(reorderedItems).asWidget().center(-1, height))
     }
-
-    private fun createInventory(items: List<ItemStack>): DisplayWidget {
-        val itemDisplays = items.chunked(9).map { chunk ->
-            chunk.map { item ->
-                Displays.padding(2, Displays.item(item, showTooltip = true, showStackSize = true))
-            }
-        }
-        val sortedItemDisplays = itemDisplays.drop(1) + itemDisplays.take(1)
-        return Displays.background(
-            SkyBlockPv.id("inventory/inventory-9x${sortedItemDisplays.size}"),
-            Displays.padding(2, sortedItemDisplays.asTable()),
-        ).asWidget()
-    }
-
-    private fun addCategories(bg: DisplayWidget) {
-        val categories = InventoryCategory.entries
-        val buttonRow = LinearLayout.horizontal().spacing(2)
-        categories.forEach { category ->
-            val button = Button()
-            button.setSize(20, 22)
-            if (category.isSelected()) {
-                button.withTexture(ExtraConstants.TAB_TOP_SELECTED)
-            } else {
-                button.withCallback { McClient.tell { McClient.setScreen(category.create(gameProfile, profile)) } }
-                button.withTexture(ExtraConstants.TAB_TOP)
-            }
-            button.withRenderer(
-                WidgetRenderers.padded(
-                    4, 0, 0, 0,
-                    WidgetRenderers.center(16, 16) { gr, ctx, _ -> gr.renderItem(category.icon, ctx.x, ctx.y) },
-                ),
-            )
-            buttonRow.addChild(button)
-        }
-        buttonRow.arrangeElements()
-        buttonRow.setPosition(bg.x + 20, bg.y - buttonRow.height)
-        buttonRow.visitWidgets(this::addRenderableWidget)
-    }
-
-    private fun List<ItemStack>?.orEmpty(size: Int) = this ?: List(size) { ItemStack.EMPTY }
 }
