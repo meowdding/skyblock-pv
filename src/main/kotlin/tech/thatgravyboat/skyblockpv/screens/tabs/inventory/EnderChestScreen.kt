367bb29a4e1984cd443bb5ebe43f9a9c279da92c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-10 11:01:54 +0100

    refactor(inventory): BasePagedInventoryScreen
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/EnderChestScreen.kt
@@ -1,51 +1,14 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
-import earth.terrarium.olympus.client.components.buttons.Button
-import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
-import net.minecraft.client.gui.layouts.LinearLayout
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
-import tech.thatgravyboat.skyblockpv.screens.elements.ExtraConstants
-import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
-import tech.thatgravyboat.skyblockpv.utils.Utils.centerHorizontally
-import tech.thatgravyboat.skyblockpv.utils.components.CarouselWidget
-import tech.thatgravyboat.skyblockpv.utils.displays.Displays
+import tech.thatgravyboat.skyblockpv.utils.displays.Display
 
-class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BaseInventoryScreen(gameProfile, profile) {
-    var page = 0
+class EnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePagedInventoryScreen(gameProfile, profile) {
+    private val enderChests get() = profile?.inventory?.enderChestPages ?: emptyList()
 
-    override fun createInventoryWidget() = LayoutBuild.vertical {
-        val inventory = profile?.inventory ?: return@vertical
-        val enderchests = inventory.enderChestPages ?: return@vertical
-        val buttonContainer = LinearLayout.horizontal().spacing(1)
+    override fun getInventories(): List<Display> = enderChests.map { createInventory(it.items.inventory) }
 
-        val carousel = CarouselWidget(
-            enderchests.map { createInventory(it.items.inventory) },
-            page,
-            246,
-        )
-
-        repeat(enderchests.size) { index ->
-            val enderChestItem = Items.ENDER_CHEST.defaultInstance
-            enderChestItem.count = index + 1
-            val itemDisplay = Displays.item(enderChestItem, showStackSize = true)
-
-            val button = Button()
-                .withSize(20, 20)
-                .withTexture(ExtraConstants.BUTTON_DARK)
-                .withRenderer(
-                    WidgetRenderers.center(16, 16) { gr, ctx, _ -> itemDisplay.render(gr, ctx.x, ctx.y) },
-                )
-                .withCallback {
-                    page = index
-                    carousel.index = index
-                }
-            buttonContainer.addChild(button)
-        }
-
-        widget(buttonContainer.centerHorizontally(uiWidth))
-        spacer(height = 10)
-        widget(carousel.centerHorizontally(uiWidth))
-    }
+    override fun getIcons() = List(enderChests.size) { Items.ENDER_CHEST.defaultInstance }
 }
