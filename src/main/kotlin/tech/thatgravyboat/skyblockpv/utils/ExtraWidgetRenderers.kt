16d4e5a0007f825081e0bb263453d94c06bf4b7f
Gravy Boat <gravy@thatgravyboat.tech>
2025-03-10 07:42:44 -0230

    feat: extra widget renderers

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/ExtraWidgetRenderers.kt
@@ -0,0 +1,17 @@
+package tech.thatgravyboat.skyblockpv.utils
+
+import earth.terrarium.olympus.client.components.base.renderer.WidgetRenderer
+import net.minecraft.client.gui.components.AbstractWidget
+import tech.thatgravyboat.skyblockpv.utils.displays.Display
+
+object ExtraWidgetRenderers {
+
+    fun <T : AbstractWidget> conditional(ifTrue: WidgetRenderer<T>, ifFalse: WidgetRenderer<T>, condition: () -> Boolean): WidgetRenderer<T> =
+        WidgetRenderer<T> { graphics, context, partialTicks ->
+            if (condition()) ifTrue.render(graphics, context, partialTicks) else ifFalse.render(graphics, context, partialTicks)
+        }
+
+    fun <T : AbstractWidget> display(display: Display) = WidgetRenderer<T> { graphics, context, partialTicks ->
+        display.render(graphics, context.x, context.y)
+    }
+}
