d35c767db77590edb1ef44d1c26402028f564850
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-11 10:26:00 +0100

    feat(inventory): backpack & accessory bag icon
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/InventoryCategory.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/InventoryCategory.kt
@@ -5,15 +5,19 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
+import tech.thatgravyboat.skyblockpv.data.SkullTextures
 import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
 
+val backpackIcon by lazy { SkullTextures.BACKPACK.createSkull() }
+val accessoryIcon by lazy { SkullTextures.ACCESSORY_BAG.createSkull() }
+
 enum class InventoryCategory(val screen: KClass<out BasePvScreen>, val icon: ItemStack) {
     INVENTORY(InventoryScreen::class, Items.CHEST.defaultInstance),
     ENDER_CHEST(EnderChestScreen::class, Items.ENDER_CHEST.defaultInstance),
-    BACKPACK(BackpackScreen::class, Items.SHULKER_BOX.defaultInstance), // todo: backpack playerhead icon
-    ACCESSORY(AccessoryScreen::class, Items.DIAMOND.defaultInstance), // todo: some actual icon
+    BACKPACK(BackpackScreen::class, backpackIcon),
+    ACCESSORY(AccessoryScreen::class, accessoryIcon),
     ;
 
     fun isSelected() = McScreen.self?.takeIf { it::class.isSubclassOf(screen) } != null
