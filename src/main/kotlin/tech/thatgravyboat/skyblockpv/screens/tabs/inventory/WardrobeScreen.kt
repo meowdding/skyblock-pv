3523914d34d1b8343939669fa8fa4a26161f128c
Gravy Boat <gravy@thatgravyboat.tech>
2025-03-11 18:31:31 -0230

    fix: make selected armor show up in wardrobe

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/WardrobeScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/WardrobeScreen.kt
@@ -1,23 +1,29 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
 import tech.thatgravyboat.skyblockpv.utils.displays.Display
 
 class WardrobeScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePagedInventoryScreen(gameProfile, profile) {
+
     private val inventory get() = profile?.inventory
-    private val activeArmor get() = inventory?.armorItems?.inventory.orEmpty(4)
+    private val activeArmor get() = inventory?.armorItems?.inventory.orEmpty(4).asReversed()
     private val wardrobe get() = inventory?.wardrobe
+    private val selected get() = wardrobe?.equippedArmor?.minus(1) ?: -1
     private val armor get() = wardrobe?.armor?.armor?.inventory?.chunked(36) ?: emptyList()
 
-    // todo: hypixel doesnt send your active wardrobe armor
-    private fun handleActiveArmor(): List<ItemStack> {
-        return emptyList()
+    override fun getInventories(): List<Display> = armor.mapIndexed { index, inventory ->
+        createInventory(if (selected != -1 && selected / 9 == index) {
+            inventory.chunked(9).mapIndexed { index, row ->
+                var row = row.toMutableList()
+                row[selected % 9] = activeArmor[index]
+                row
+            }.flatten()
+        } else {
+            inventory
+        })
     }
-
-    override fun getInventories(): List<Display> = armor.map { createInventory(it) }
 
     override fun getIcons() = List(armor.size) { Items.LEATHER_CHESTPLATE.defaultInstance }
 }
