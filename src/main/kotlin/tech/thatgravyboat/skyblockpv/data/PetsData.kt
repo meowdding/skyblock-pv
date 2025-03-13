1bbb1da18ac049a2cea6bce5bcf2dc7e4ac7fa4d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-13 17:33:43 +0100

    chore: parse pets
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/data/PetsData.kt
@@ -0,0 +1,13 @@
+package tech.thatgravyboat.skyblockpv.data
+
+data class Pet(
+    val uuid: String?,
+    val uniqueId: String, // TODO: whats the difference? why can one be null? what the fuck hypixel
+    val type: String,
+    val exp: Long,
+    val active: Boolean,
+    val tier: String,
+    val heldItem: String?,
+    val candyUsed: Int,
+    val skin: String?,
+)
