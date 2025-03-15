cdd0f5f85bec6a6b5a46b495b8bad122bb4f65ed
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-15 20:29:17 +0100

    refactor: chatutils
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/api/StatusAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/api/StatusAPI.kt
@@ -1,8 +1,7 @@
 package tech.thatgravyboat.skyblockpv.api
 
-import tech.thatgravyboat.skyblockapi.utils.text.Text
-import tech.thatgravyboat.skyblockapi.utils.text.Text.send
 import tech.thatgravyboat.skyblockpv.api.data.PlayerStatus
+import tech.thatgravyboat.skyblockpv.utils.ChatUtils
 import java.util.*
 
 private const val PATH = "v2/status"
@@ -10,7 +9,7 @@ private const val PATH = "v2/status"
 object StatusAPI {
     suspend fun getStatus(uuid: UUID): PlayerStatus? {
         val result = HypixelAPI.get(PATH, mapOf("uuid" to uuid.toString())) ?: run {
-            Text.of("Something went wrong :3").send()
+            ChatUtils.chat("Something went wrong :3")
             return null
         }
         return PlayerStatus.fromJson(result)
