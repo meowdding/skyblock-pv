91403a8a58c22271eb2771e2ebdaf8e001ff9339
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-17 21:42:31 +0100

    feat: half working pet screen
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/PetScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/PetScreen.kt
@@ -1,38 +1,76 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs
 
 import com.mojang.authlib.GameProfile
+import earth.terrarium.olympus.client.components.buttons.Button
+import earth.terrarium.olympus.client.layouts.Layouts
+import earth.terrarium.olympus.client.layouts.LinearViewLayout
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.client.gui.layouts.Layout
 import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
-import tech.thatgravyboat.skyblockapi.utils.text.Text
-import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+import tech.thatgravyboat.skyblockpv.SkyBlockPv
 import tech.thatgravyboat.skyblockpv.api.ItemAPI
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
 import tech.thatgravyboat.skyblockpv.data.Pet
 import tech.thatgravyboat.skyblockpv.data.SortedEntry
 import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
+import tech.thatgravyboat.skyblockpv.utils.ExtraWidgetRenderers
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuilder.Companion.setPos
+import tech.thatgravyboat.skyblockpv.utils.Utils
+import tech.thatgravyboat.skyblockpv.utils.Utils.asScrollable
 import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
 import tech.thatgravyboat.skyblockpv.utils.displays.Displays
 
 class PetScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BasePvScreen("PETS", gameProfile, profile) {
 
+    private var activePet: Pet? = null
+
     override fun create(bg: DisplayWidget) {
         val pets = profile?.pets ?: return
-        val sortedPets = pets.sortedWith(compareBy({ SortedEntry.RARITY.list.indexOf(it.tier) }, { it.exp }))
-
-        val petLayouts = sortedPets.map { createPetLayout(it) }.chunked(20)
+        val sortedPets = pets.sortedWith(compareBy<Pet> { SortedEntry.RARITY.list.indexOf(it.tier) }.thenByDescending { it.exp })
+        val leftColumnWidth = ((bg.width - 10) * 0.65).toInt()
+        val rightColumnWidth = (bg.width - 10 - leftColumnWidth).toInt()
 
         LayoutBuild.horizontal(5) {
-            widget(petLayouts.map { LayoutBuild.vertical { widget(it) } })
-        }.setPos(bg.x + 5, bg.y + 5).visitWidgets(this::addRenderableOnly)
+            widget(createPetRow(sortedPets, leftColumnWidth).asScrollable(leftColumnWidth, uiHeight - 10))
+            widget(createInfoRow(rightColumnWidth))
+        }.setPos(bg.x + 5, bg.y + 5).visitWidgets(this::addRenderableWidget)
     }
 
-    private fun createPetLayout(pet: Pet) = LayoutBuild.horizontal(3, 0.5f) {
-        val rarity = SkyBlockRarity.entries.find { it.name == pet.tier } ?: SkyBlockRarity.COMMON
-        val itemStack = ItemAPI.getPet(pet.type, rarity, skin = pet.skin)
+    private fun createPetRow(pets: List<Pet>, width: Int): Layout {
+        val amountPerRow = width / 28
 
-        display(Displays.item(itemStack, showTooltip = true))
-        string(Text.of(pet.type) { color = rarity.color })
+        val petLayouts = pets.map { createPetLayout(it) }
+        val petContainer = petLayouts.chunked(amountPerRow)
+            .map { it.fold(Layouts.row().withGap(5), LinearViewLayout::withChild) }
+            .fold(Layouts.column().withGap(5), LinearViewLayout::withChild)
+        petContainer.arrangeElements()
+        return petContainer
+    }
+
+    private fun createPetLayout(pet: Pet): AbstractWidget {
+        val rarity = SkyBlockRarity.entries.find { it.name == pet.tier } ?: SkyBlockRarity.COMMON
+        val itemStack = ItemAPI.getPet(pet.type, rarity, pet.level, pet.skin)
+
+        val itemDisplay = Displays.item(itemStack, showTooltip = true, customStackText = "${pet.level}")
+        val display = Displays.background(SkyBlockPv.id("inventory/inventory-1x1"), Displays.padding(3, itemDisplay))
+        return Button()
+            .withSize(22, 22)
+            .withTexture(null)
+            .withRenderer(ExtraWidgetRenderers.display(display))
+            .withCallback {
+                activePet = pet
+                this.rebuildWidgets()
+            }
+    }
+
+    private fun createInfoRow(width: Int) = LayoutBuild.vertical {
+        val petInfo = LayoutBuild.vertical {
+            string("Name: ${activePet?.type ?: "None"}")
+        }
+
+        widget(Utils.getTitleWidget("Pet Info", width))
+        widget(Utils.getMainContentWidget(petInfo, width))
     }
 }
 
