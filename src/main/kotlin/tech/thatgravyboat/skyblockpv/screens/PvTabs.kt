033a97aeb7cc655d8c7944d6e54878ddd41d5081
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-17 15:42:39 +0100

    chore: load pets
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/PvTabs.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/PvTabs.kt
@@ -5,10 +5,10 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
-import tech.thatgravyboat.skyblockpv.screens.tabs.*
 import tech.thatgravyboat.skyblockpv.screens.tabs.CollectionScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.FishingScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.MainScreen
+import tech.thatgravyboat.skyblockpv.screens.tabs.PetScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.combat.BaseCombatScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.combat.DungeonScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.inventory.BaseInventoryScreen
@@ -22,7 +22,7 @@ import kotlin.reflect.full.isSubclassOf
 enum class PvTabs(
     private val screen: KClass<out BasePvScreen>,
     private val constructor: (GameProfile, SkyBlockProfile?) -> BasePvScreen,
-    private val icon: (GameProfile?) -> ItemStack
+    private val icon: (GameProfile?) -> ItemStack,
 ) {
     MAIN(MainScreen::class, ::MainScreen, { it?.let(::createSkull) ?: Items.PLAYER_HEAD.defaultInstance }),
     COMBAT(BaseCombatScreen::class, ::DungeonScreen, Items.DIAMOND_SWORD.defaultInstance),
