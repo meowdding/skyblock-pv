3e5a87fe8fdaadb9935c3a59c98141f290a96fa8
mona <59416038+meowora@users.noreply.github.com>
2025-03-26 19:59:44 +0100

    fix tooltips not showing

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/ItemUtils.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/ItemUtils.kt
@@ -5,9 +5,9 @@ import com.google.gson.JsonObject
 import com.mojang.authlib.GameProfile
 import com.mojang.authlib.properties.Property
 import com.mojang.serialization.Dynamic
-import it.unimi.dsi.fastutil.objects.ReferenceSortedSets
 import net.azureaaron.legacyitemdfu.LegacyItemStackFixer
 import net.azureaaron.legacyitemdfu.TypeReferences
+import net.minecraft.core.component.DataComponentType
 import net.minecraft.core.component.DataComponents
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NbtOps
@@ -21,6 +21,35 @@ import net.minecraft.world.item.component.ResolvableProfile
 import net.minecraft.world.item.component.TooltipDisplay
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import java.util.*
+
+
+val tooltipDataComponents = LinkedHashSet<DataComponentType<*>>(
+    listOf(
+        DataComponents.TROPICAL_FISH_PATTERN,
+        DataComponents.INSTRUMENT,
+        DataComponents.MAP_ID,
+        DataComponents.BEES,
+        DataComponents.CONTAINER_LOOT,
+        DataComponents.CONTAINER,
+        DataComponents.BANNER_PATTERNS,
+        DataComponents.POT_DECORATIONS,
+        DataComponents.WRITTEN_BOOK_CONTENT,
+        DataComponents.CHARGED_PROJECTILES,
+        DataComponents.FIREWORKS,
+        DataComponents.FIREWORK_EXPLOSION,
+        DataComponents.POTION_CONTENTS,
+        DataComponents.JUKEBOX_PLAYABLE,
+        DataComponents.TRIM,
+        DataComponents.STORED_ENCHANTMENTS,
+        DataComponents.ENCHANTMENTS,
+        DataComponents.DYED_COLOR,
+        DataComponents.OMINOUS_BOTTLE_AMPLIFIER,
+        DataComponents.SUSPICIOUS_STEW_EFFECTS,
+        DataComponents.BLOCK_STATE,
+        DataComponents.ATTRIBUTE_MODIFIERS,
+        DataComponents.UNBREAKABLE
+    ),
+)
 
 fun createSkull(textureBase64: String): ItemStack {
     val profile = GameProfile(UUID.randomUUID(), "a")
@@ -37,10 +66,9 @@ fun createSkull(profile: GameProfile): ItemStack {
 fun ItemStack.getLore(): List<Component> = this[DataComponents.LORE]?.lines ?: emptyList()
 
 fun Tag.legacyStack(): ItemStack {
-    val ops: RegistryOps<Tag>? = McClient.self.connection?.registryAccess()?.createSerializationContext(NbtOps.INSTANCE)
-    if (ops == null) return ItemStack.EMPTY
+    val ops: RegistryOps<Tag> = McClient.self.connection?.registryAccess()?.createSerializationContext(NbtOps.INSTANCE) ?: return ItemStack.EMPTY
     val fixed: Dynamic<Tag> = LegacyItemStackFixer.getFixer().update(
-        TypeReferences.LEGACY_ITEM_STACK, Dynamic<Tag>(ops, this),
+        TypeReferences.LEGACY_ITEM_STACK, Dynamic(ops, this),
         LegacyItemStackFixer.getFirstVersion(),
         LegacyItemStackFixer.getLatestVersion(),
     ).map {
@@ -54,12 +82,9 @@ fun Tag.legacyStack(): ItemStack {
         }
         return@map it
     }
-    val stack = ItemStack.CODEC.parse<Tag>(fixed)
-        .setPartial(ItemStack.EMPTY)
-        .resultOrPartial()
-        .get()
+    val stack = ItemStack.CODEC.parse(fixed).setPartial(ItemStack.EMPTY).resultOrPartial().get()
 
-    stack.set(DataComponents.TOOLTIP_DISPLAY, TooltipDisplay(true, ReferenceSortedSets.emptySet()),)
+    stack.set(DataComponents.TOOLTIP_DISPLAY, TooltipDisplay(false, tooltipDataComponents))
     stack.set(DataComponents.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers(listOf()))
 
     return stack
