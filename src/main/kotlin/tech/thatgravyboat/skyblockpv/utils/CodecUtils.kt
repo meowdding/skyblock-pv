d1367f242b3ed8d4df2c58e66d75fa93c4291e24
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-26 17:03:40 +0100

    style: formatting
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/CodecUtils.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/utils/CodecUtils.kt
@@ -10,7 +10,7 @@ object CodecUtils {
     val CUMULATIVE_INT_LIST: Codec<List<Int>> =
         Codec.INT.listOf().xmap(
             { it.runningFold(0, Int::plus).distinct() },
-            { it.reversed().runningFold(0, Int::minus).reversed() }
+            { it.reversed().runningFold(0, Int::minus).reversed() },
         )
 
     val VECTOR_2I = Codec.INT.listOf(2, 2).xmap(
@@ -24,13 +24,21 @@ object CodecUtils {
     )
 
     val CUMULATIVE_STRING_INT_MAP: Codec<List<Map<String, Int>>> = Codec.unboundedMap(Codec.STRING, Codec.INT).listOf()
-        .xmap({
-            it.runningFold(mutableMapOf(), { acc: MutableMap<String, Int>, mutableMap: MutableMap<String, Int>? ->
-                LinkedHashMap(acc.also {
-                    mutableMap?.forEach {
-                        acc[it.key] = it.value +(acc[it.key] ?: 0)
-                    }
-                })
-            }).drop(1)
-        }, {it})
+        .xmap(
+            {
+                it.runningFold(
+                    mutableMapOf(),
+                    { acc: MutableMap<String, Int>, mutableMap: MutableMap<String, Int>? ->
+                        LinkedHashMap(
+                            acc.also {
+                                mutableMap?.forEach {
+                                    acc[it.key] = it.value + (acc[it.key] ?: 0)
+                                }
+                            },
+                        )
+                    },
+                ).drop(1)
+            },
+            { it },
+        )
 }
