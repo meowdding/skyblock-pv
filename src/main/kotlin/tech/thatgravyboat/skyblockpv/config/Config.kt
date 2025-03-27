9a9d7d574f8faf687b58392b5b7499a5a2e433e3
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-27 20:00:12 +0100

    feat: config
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/config/Config.kt
@@ -0,0 +1,34 @@
+package tech.thatgravyboat.skyblockpv.config
+
+import com.teamresourceful.resourcefulconfig.api.types.info.ResourcefulConfigLink
+import com.teamresourceful.resourcefulconfig.api.types.options.TranslatableValue
+import com.teamresourceful.resourcefulconfigkt.api.ConfigKt
+
+object Config : ConfigKt("skyblockpv/config") {
+
+    override val name get() = TranslatableValue("SkyBlock Profile Viewer")
+    override val description get() = TranslatableValue("Making Modern Pv'able")
+    override val links: Array<ResourcefulConfigLink>
+        get() = arrayOf(
+            ResourcefulConfigLink.create(
+                "https://discord.gg/FsRc2GUwZR",
+                "discord",
+                TranslatableValue("Discord"),
+            ),
+            ResourcefulConfigLink.create(
+                "https://modrinth.com/project/skyblock-profile-viewer",
+                "modrinth",
+                TranslatableValue("Modrinth"),
+            ),
+            ResourcefulConfigLink.create(
+                "https://github.com/meowdding/skyblock-pv",
+                "code",
+                TranslatableValue("GitHub"),
+            ),
+        )
+
+    var devMode by boolean(false) {
+        name = Translated("sbpv.dev")
+        description = Translated("sbpv.dev.desc")
+    }
+}
