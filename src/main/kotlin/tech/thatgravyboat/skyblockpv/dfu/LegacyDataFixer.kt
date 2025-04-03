8a54b6770977fff06fa2c3f3a067ec7404f85b8b
Gravy Boat <gravy@thatgravyboat.tech>
2025-04-03 19:38:09 -0230

    feat: damage parsing

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/dfu/LegacyDataFixer.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/dfu/LegacyDataFixer.kt
@@ -2,8 +2,8 @@ package tech.thatgravyboat.skyblockpv.dfu
 
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.Tag
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import tech.thatgravyboat.skyblockpv.dfu.base.BaseItem
 import tech.thatgravyboat.skyblockpv.dfu.fixes.*
 import tech.thatgravyboat.skyblockpv.dfu.fixes.display.ColorFixer
 import tech.thatgravyboat.skyblockpv.dfu.fixes.display.LoreFixer
@@ -12,7 +12,6 @@ import tech.thatgravyboat.skyblockpv.dfu.fixes.display.NameFixer
 object LegacyDataFixer {
 
     private val fixers = listOf(
-        ItemCountFixer,
         HideFlagsFixer,
         SkullTextureFixer,
         LoreFixer,
@@ -32,20 +31,14 @@ object LegacyDataFixer {
 
         if (tag.isEmpty) return ItemStack.EMPTY
 
-        val item: Item
-        try {
-            item = FlatteningFixer.getItem(tag)
-        } catch (e: NullPointerException) {
-            return ItemStack.EMPTY
-        }
+        val base = BaseItem.getBase(tag) ?: error("Base item not found")
 
-        val defaultInstance = item.defaultInstance
         fixers.forEach {
-            if (it.shouldApply(defaultInstance)) {
-                it.fixItem(defaultInstance, tag)
+            if (it.shouldApply(base)) {
+                it.fixItem(base, tag)
             }
         }
 
-        return defaultInstance
+        return base
     }
 }
