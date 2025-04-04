93839d4c7828aa4d2c1ee66bf3be5ef423d162f4
mona <59416038+meowora@users.noreply.github.com>
2025-04-04 23:34:54 +0200

    feat: add firework fixer

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/dfu/fixes/FireworkExplosionFixer.kt
@@ -0,0 +1,37 @@
+package tech.thatgravyboat.skyblockpv.dfu.fixes
+
+import it.unimi.dsi.fastutil.ints.IntArrayList
+import it.unimi.dsi.fastutil.ints.IntList
+import it.unimi.dsi.fastutil.ints.IntLists
+import net.minecraft.core.component.DataComponentType
+import net.minecraft.core.component.DataComponents
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.item.component.FireworkExplosion
+import tech.thatgravyboat.skyblockpv.dfu.DataComponentFixer
+
+object FireworkExplosionFixer : DataComponentFixer<FireworkExplosion> {
+    override val type: DataComponentType<FireworkExplosion> = DataComponents.FIREWORK_EXPLOSION
+
+    override fun getData(tag: CompoundTag): FireworkExplosion? {
+        val compoundTag = tag.getAndRemoveCompound("Explosion") ?: return null
+        val explosionShape = getExplosionById(compoundTag.getAndRemoveInt("Type") ?: 0)
+        val colors = compoundTag.getAndRemoveIntArray("Colors").toIntList()
+        val fadeColors = compoundTag.getAndRemoveIntArray("FadeColors").toIntList()
+        val hasTrail = compoundTag.getAndRemoveBoolean("Trail") ?: false
+        val hasTwinkle = compoundTag.getAndRemoveBoolean("Flicker") ?: false
+
+        return FireworkExplosion(explosionShape, colors, fadeColors, hasTrail, hasTwinkle)
+    }
+
+    private fun IntArray?.toIntList(): IntList {
+        return this?.let { IntArrayList(it) } ?: IntLists.EMPTY_LIST
+    }
+
+    private fun getExplosionById(id: Int): FireworkExplosion.Shape = when (id) {
+        1 -> FireworkExplosion.Shape.LARGE_BALL
+        2 -> FireworkExplosion.Shape.STAR
+        3 -> FireworkExplosion.Shape.CREEPER
+        4 -> FireworkExplosion.Shape.BURST
+        else -> FireworkExplosion.Shape.SMALL_BALL
+    }
+}
