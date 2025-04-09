1ba783b768aeb12b3d6bffae2d72d15ae6bfb5db
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-09 18:59:16 +0200

    refactor: cleanup enus
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/feature/debug/SacksParser.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/feature/debug/SacksParser.kt
@@ -2,6 +2,7 @@ package tech.thatgravyboat.skyblockpv.feature.debug
 
 import com.google.gson.GsonBuilder
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
@@ -11,11 +12,12 @@ import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.match
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.Text.send
 import tech.thatgravyboat.skyblockpv.SkyBlockPv
+import tech.thatgravyboat.skyblockpv.api.ItemAPI
 import tech.thatgravyboat.skyblockpv.config.DevConfig
 import java.nio.file.Files
 
 object SacksParser {
-    private var data: MutableMap<String, MutableSet<String>> = mutableMapOf()
+    private var data: MutableMap<String, Sack> = mutableMapOf()
 
     private var titleRegex = "(?<name>.*) Sack".toRegex()
 
@@ -26,8 +28,9 @@ object SacksParser {
 
         val id = event.item.getData(DataTypes.ID) ?: return
         titleRegex.match(event.title, "name") { (name) ->
+            val title = event.title.replace("Gemstones", "Gemstone")
             val parsedIds = when (name) {
-                "Gemstone" -> {
+                "Gemstones" -> {
                     listOf("ROUGH", "FLAWED", "FINE").map {
                         id.replace("ROUGH", it)
                     }
@@ -36,7 +39,11 @@ object SacksParser {
                 else -> listOf(id)
             }
 
-            data.computeIfAbsent(name) { mutableSetOf() }.addAll(parsedIds)
+            val sackId = listOf("", "LARGE_", "MEDIUM_", "SMALL_").firstNotNullOfOrNull {
+                ItemAPI.getItem("$it${title.replace(" ", "_").uppercase()}").takeUnless { it.item == Items.BARRIER }?.getData(DataTypes.ID)
+            } ?: title
+
+            data.computeIfAbsent(name) { Sack(sackId, mutableListOf()) }.add(parsedIds)
         }
     }
 
@@ -52,9 +59,16 @@ object SacksParser {
 
         Files.createDirectories(file.parent)
         Files.writeString(file, GsonBuilder().create().toJson(data))
-
-        data.clear()
     }
 
     fun shouldParse() = SkyBlockPv.isDevMode && DevConfig.sacksParser
+
+    private data class Sack(
+        val sack: String,
+        val items: MutableList<String>,
+    ) {
+        fun add(list: List<String>) {
+            items.addAll(list)
+        }
+    }
 }
