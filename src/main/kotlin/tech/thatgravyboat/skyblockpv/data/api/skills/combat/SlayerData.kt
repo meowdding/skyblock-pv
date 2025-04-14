5467ea2c8a74dee4e2a0ca2d644a2c7985b32c33
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-14 18:05:49 +0200

    refactor: remove unused
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/data/api/skills/combat/SlayerData.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/data/api/skills/combat/SlayerData.kt
@@ -3,27 +3,6 @@ package tech.thatgravyboat.skyblockpv.data.api.skills.combat
 import net.minecraft.resources.ResourceLocation
 import tech.thatgravyboat.skyblockpv.SkyBlockPv
 
-private val xpPerLevelNormal = mapOf(
-    1 to 10L,
-    2 to 30L,
-    3 to 250L,
-    4 to 1_500L,
-    5 to 5_000L,
-    6 to 20_000L,
-    7 to 100_000L,
-    8 to 400_000L,
-    9 to 1_000_000L,
-)
-
-private val xpPerLevelVampire = mapOf(
-    1 to 20L,
-    2 to 75L,
-    3 to 240L,
-    4 to 840L,
-    5 to 2_400L,
-)
-
-
 fun getIconFromSlayerName(name: String): ResourceLocation = SkyBlockPv.id(
     when (name) {
         "revenant" -> "icon/slayer/revenant"
@@ -35,15 +14,6 @@ fun getIconFromSlayerName(name: String): ResourceLocation = SkyBlockPv.id(
         else -> "icon/questionmark"
     },
 )
-
-fun getSlayerLevel(slayer: String, xp: Long): Int {
-    val xpPerLevel = when (slayer) {
-        "vampire" -> xpPerLevelVampire
-        else -> xpPerLevelNormal
-    }
-
-    return xpPerLevel.entries.lastOrNull { it.value < xp }?.key ?: 0
-}
 
 data class SlayerTypeData(
     val exp: Long,
