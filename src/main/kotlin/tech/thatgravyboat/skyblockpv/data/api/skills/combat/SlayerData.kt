2d1cf90d76881294ee9a1bd8603ee68652ef53d9
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-15 12:03:14 +0200

    refactor: improve get slayer icon
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/data/api/skills/combat/SlayerData.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/data/api/skills/combat/SlayerData.kt
@@ -3,17 +3,7 @@ package tech.thatgravyboat.skyblockpv.data.api.skills.combat
 import net.minecraft.resources.ResourceLocation
 import tech.thatgravyboat.skyblockpv.SkyBlockPv
 
-fun getIconFromSlayerName(name: String): ResourceLocation = SkyBlockPv.id(
-    when (name) {
-        "revenant" -> "icon/slayer/revenant"
-        "tarantula" -> "icon/slayer/tarantula"
-        "sven" -> "icon/slayer/sven"
-        "voidgloom" -> "icon/slayer/voidgloom"
-        "inferno_demonlord" -> "icon/slayer/inferno"
-        "vampire" -> "icon/slayer/vampire"
-        else -> "icon/questionmark"
-    },
-)
+fun getIconFromSlayerName(name: String): ResourceLocation = SkyBlockPv.id("icon/slayer/$name")
 
 data class SlayerTypeData(
     val exp: Long,
