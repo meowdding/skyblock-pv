1178b0ad201695ae357b66025127a7f41198edb8
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-17 12:01:45 +0200

    refactor: dedupe carousel icon code
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/rift/RiftEnderChestScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/rift/RiftEnderChestScreen.kt
@@ -1,14 +1,8 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs.rift
 
 import com.mojang.authlib.GameProfile
-import earth.terrarium.olympus.client.components.buttons.Button
-import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
-import earth.terrarium.olympus.client.layouts.Layouts
-import earth.terrarium.olympus.client.layouts.LinearViewLayout
-import earth.terrarium.olympus.client.ui.UIConstants
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
-import tech.thatgravyboat.skyblockpv.utils.ExtraWidgetRenderers
 import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
 import tech.thatgravyboat.skyblockpv.utils.LayoutUtils.centerHorizontally
 import tech.thatgravyboat.skyblockpv.utils.components.CarouselWidget
@@ -30,30 +24,13 @@ class RiftEnderChestScreen(gameProfile: GameProfile, profile: SkyBlockProfile? =
             246,
         )
 
-        val buttons = List(inventories.size) { index ->
-            val icon = icons[index]
-            icon.count = index + 1
-            val itemDisplay = Displays.item(icon, showStackSize = true)
-
-            Button()
-                .withSize(20, 20)
-                .withRenderer(
-                    WidgetRenderers.layered(
-                        ExtraWidgetRenderers.conditional(
-                            WidgetRenderers.sprite(UIConstants.PRIMARY_BUTTON),
-                            WidgetRenderers.sprite(UIConstants.DARK_BUTTON),
-                        ) { index == carousel?.index },
-                        WidgetRenderers.center(16, 20, ExtraWidgetRenderers.display(itemDisplay)),
-                    ),
-                )
-                .withCallback {
-                    carousel?.index = index
-                }
+        val buttonContainer = carousel!!.getIcons {
+            List(inventories.size) { index ->
+                val icon = icons[index]
+                icon.count = index + 1
+                Displays.item(icon, showStackSize = true)
+            }
         }
-
-        val buttonContainer = buttons.chunked(9)
-            .map { it.fold(Layouts.row().withGap(1), LinearViewLayout::withChild) }
-            .fold(Layouts.column().withGap(1), LinearViewLayout::withChild)
 
         widget(buttonContainer.centerHorizontally(uiWidth))
         spacer(height = 10)
