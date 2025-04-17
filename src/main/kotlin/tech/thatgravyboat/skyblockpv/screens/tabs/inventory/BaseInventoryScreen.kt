f9b0bc55f24799c5791e125914ccb676def72931
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-17 10:31:42 +0200

    refactor: move base inv screens & categories into one file
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/BaseInventoryScreen.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockpv/screens/tabs/inventory/BaseInventoryScreen.kt
@@ -1,30 +1,104 @@
 package tech.thatgravyboat.skyblockpv.screens.tabs.inventory
 
 import com.mojang.authlib.GameProfile
+import earth.terrarium.olympus.client.components.buttons.Button
+import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
+import earth.terrarium.olympus.client.layouts.Layouts
+import earth.terrarium.olympus.client.layouts.LinearViewLayout
+import earth.terrarium.olympus.client.ui.UIConstants
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockpv.api.data.SkyBlockProfile
+import tech.thatgravyboat.skyblockpv.data.repo.SkullTextures
+import tech.thatgravyboat.skyblockpv.screens.BasePvScreen
 import tech.thatgravyboat.skyblockpv.screens.tabs.base.AbstractCategorizedScreen
+import tech.thatgravyboat.skyblockpv.screens.tabs.base.Category
+import tech.thatgravyboat.skyblockpv.utils.ExtraWidgetRenderers
+import tech.thatgravyboat.skyblockpv.utils.LayoutBuild
+import tech.thatgravyboat.skyblockpv.utils.LayoutUtils.centerHorizontally
+import tech.thatgravyboat.skyblockpv.utils.components.CarouselWidget
 import tech.thatgravyboat.skyblockpv.utils.displays.Display
+import tech.thatgravyboat.skyblockpv.utils.displays.DisplayWidget
 import tech.thatgravyboat.skyblockpv.utils.displays.Displays
-import tech.thatgravyboat.skyblockpv.utils.displays.asTable
+import tech.thatgravyboat.skyblockpv.utils.displays.asWidget
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+
+enum class InventoryCategory(val screen: KClass<out BasePvScreen>, override val icon: ItemStack) : Category {
+    INVENTORY(InventoryScreen::class, Items.CHEST.defaultInstance),
+    ENDER_CHEST(EnderChestScreen::class, Items.ENDER_CHEST.defaultInstance),
+    BACKPACK(BackpackScreen::class, SkullTextures.BACKPACK.skull),
+    WARDROBE(WardrobeScreen::class, Items.LEATHER_CHESTPLATE.defaultInstance),
+    ACCESSORY(AccessoryScreen::class, SkullTextures.ACCESSORY_BAG.skull),
+    SACKS(SacksScreen::class, SkullTextures.SACKS.skull),
+    PERSONAL_VAULT(ItemVaultScreen::class, SkullTextures.PERSONAL_VAULT.skull),
+    POTION_BAG(PotionBagScreen::class, Items.POTION.defaultInstance),
+    FISHING_BAG(FishingBagScreen::class, Items.FISHING_ROD.defaultInstance),
+    QUIVER_BAG(QuiverBagScreen::class, Items.ARROW.defaultInstance),
+    ;
+
+    override val isSelected: Boolean get() = McScreen.self?.takeIf { it::class.isSubclassOf(screen) } != null
+    override fun create(gameProfile: GameProfile, profile: SkyBlockProfile?) = screen.constructors.first().call(gameProfile, profile)
+}
 
 abstract class BaseInventoryScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : AbstractCategorizedScreen("INVENTORY", gameProfile, profile) {
     override val categories get() = InventoryCategory.entries
+    protected fun List<ItemStack>?.orEmpty(size: Int) = this ?: List(size) { ItemStack.EMPTY }
+}
 
-    @Deprecated("Use the one in PvWidgets", ReplaceWith("PvWidgets.createInventory(items)"))
-    protected fun createInventory(items: List<ItemStack>): Display {
-        val itemDisplays = items.chunked(9).map { chunk ->
-            val updatedChunk = chunk + List(9 - chunk.size) { ItemStack.EMPTY }
-            updatedChunk.map { item ->
-                Displays.padding(2, Displays.item(item, showTooltip = true, showStackSize = true))
-            }
+abstract class BasePagedInventoryScreen(gameProfile: GameProfile, profile: SkyBlockProfile? = null) : BaseInventoryScreen(gameProfile, profile) {
+
+    protected var carousel: CarouselWidget? = null
+
+    abstract fun getInventories(): List<Display>
+    abstract fun getIcons(): List<ItemStack>
+    open fun getExtraLine(): Display? = null
+
+    open val itemStackSize = true
+
+    override fun getLayout(bg: DisplayWidget) = LayoutBuild.vertical {
+        val inventories = getInventories()
+        val icons = getIcons()
+
+        carousel = CarouselWidget(
+            inventories,
+            carousel?.index ?: 0,
+            246,
+        )
+
+        val buttons = List(inventories.size) { index ->
+            val icon = icons[index]
+            if (itemStackSize) icon.count = index + 1
+            val itemDisplay = Displays.item(icon, showStackSize = true)
+
+            Button()
+                .withSize(20, 20)
+                .withRenderer(
+                    WidgetRenderers.layered(
+                        ExtraWidgetRenderers.conditional(
+                            WidgetRenderers.sprite(UIConstants.PRIMARY_BUTTON),
+                            WidgetRenderers.sprite(UIConstants.DARK_BUTTON),
+                        ) { index == carousel?.index },
+                        WidgetRenderers.center(16, 20, ExtraWidgetRenderers.display(itemDisplay)),
+                    ),
+                )
+                .withCallback {
+                    carousel?.index = index
+                }
         }
 
-        return Displays.inventoryBackground(
-            9, itemDisplays.size,
-            Displays.padding(2, itemDisplays.asTable()),
-        )
-    }
+        val buttonContainer = buttons.chunked(9)
+            .map { it.fold(Layouts.row().withGap(1), LinearViewLayout::withChild) }
+            .fold(Layouts.column().withGap(1), LinearViewLayout::withChild)
 
-    protected fun List<ItemStack>?.orEmpty(size: Int) = this ?: List(size) { ItemStack.EMPTY }
+        widget(buttonContainer.centerHorizontally(uiWidth))
+        spacer(height = 10)
+        widget(carousel!!.centerHorizontally(uiWidth))
+
+        getExtraLine()?.let {
+            spacer(height = 5)
+            widget(it.asWidget().centerHorizontally(uiWidth))
+        }
+    }
 }
