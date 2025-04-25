86e089b1472458a05d29ceda2401b25665e02e0e
Gravy Boat <gravy@thatgravyboat.tech>
2025-04-25 07:43:19 -0230

    feat: add cache bypassing

--- src/main/kotlin/me/owdding/skyblockpv/api/PvAPI.kt
+++ src/main/kotlin/me/owdding/skyblockpv/api/PvAPI.kt
@@ -4,16 +4,23 @@ import com.google.gson.JsonObject
 import me.owdding.skyblockpv.SkyBlockPv
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.utils.http.Http
+import tech.thatgravyboat.skyblockapi.utils.json.Json
 import java.util.*
 
 private const val API_URL = "https://skyblock-pv.thatgravyboat.tech%s"
-// TODO im lazy and dont want to reauth after a day
 
-object HypixelAPI {
+private const val AUTHENTICATION_FAILED_MESSAGE = "Failed to authenticate with PV API. Please restart the game and try again."
+
+object PvAPI {
 
     private var key: String? = null
+    private var failedReauth: Boolean = false
 
-    suspend fun authenticate() {
+    fun isAuthenticated(): Boolean {
+        return key != null && !failedReauth
+    }
+
+    suspend fun authenticate(bypassCaches: Boolean = false) {
         val server = UUID.randomUUID().toString()
         val user = McClient.self.user
 
@@ -21,9 +28,11 @@ object HypixelAPI {
 
         val response = Http.get<String>(
             url = API_URL.format("/authenticate"),
-//             queries = mapOf(
-//                 "bypassCache" to "true"
-//             ),
+            queries = if (bypassCaches) {
+                mapOf("bypassCache" to "true")
+            } else {
+                emptyMap()
+            },
             headers = mapOf(
                 "User-Agent" to "SkyBlockPV (${SkyBlockPv.version.friendlyString})",
                 "x-minecraft-username" to user.name,
@@ -35,24 +44,34 @@ object HypixelAPI {
     }
 
     suspend fun get(endpoint: String): JsonObject? {
-        if (this.key == null) {
-            SkyBlockPv.error("Hypixel API key is not set. Please authenticate first.")
+        if (this.key == null || failedReauth) {
+            SkyBlockPv.error(AUTHENTICATION_FAILED_MESSAGE)
             return null
         }
 
-        val response = Http.getResult<JsonObject>(
+        val response = Http.get(
             url = API_URL.format(endpoint),
             headers = mapOf(
                 "User-Agent" to "SkyBlockPV (${SkyBlockPv.version.friendlyString})",
                 "Authorization" to this.key!!,
             ),
-        ).map { it.takeIf { it.get("success")?.asBoolean == true } }
+            handler = { this }
+        )
 
-        if (response.isFailure) {
-            SkyBlockPv.error("Failed to get data from Hypixel API:", response.exceptionOrNull())
-            return null
+        if (response.isOk) {
+            return response.asJson(Json.gson)
+        } else if (response.statusCode == 401) {
+            authenticate()
+            if (this.key != null) {
+                return get(endpoint)
+            } else {
+                failedReauth = true
+                SkyBlockPv.error(AUTHENTICATION_FAILED_MESSAGE)
+            }
+        } else {
+            SkyBlockPv.error("Failed to fetch data from Hypixel API: ${response.statusCode}")
         }
 
-        return response.getOrNull()
+        return null
     }
 }
--- src/main/kotlin/me/owdding/skyblockpv/config/DevConfig.kt
+++ src/main/kotlin/me/owdding/skyblockpv/config/DevConfig.kt
@@ -1,8 +1,19 @@
 package me.owdding.skyblockpv.config
 
 import com.teamresourceful.resourcefulconfigkt.api.CategoryKt
+import kotlinx.coroutines.runBlocking
+import me.owdding.skyblockpv.api.PvAPI
+import net.minecraft.client.gui.components.toasts.SystemToast
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.Text
 
 object DevConfig : CategoryKt("Dev") {
+
+    private val SUPER_USERS = setOf(
+        "503450fc-72c2-4e87-8243-94e264977437",
+        "e90ea9ec-080a-401b-8d10-6a53c407ac53",
+        "b75d7e0a-03d0-4c2a-ae47-809b6b808246"
+    )
 
     var devMode by boolean(false) {
         name = Translated("skyblockpv.dev.dev_mode")
@@ -19,4 +30,33 @@ object DevConfig : CategoryKt("Dev") {
         description = Translated("skyblockpv.dev.sacks_parser.desc")
     }
 
+    init {
+        if (McClient.self.user.profileId.toString() in SUPER_USERS) {
+            button {
+                title = "skyblockpv.dev.bypass_cache"
+                description = "skyblockpv.dev.bypass_cache.desc"
+                text = "Bypass Cache"
+                onClick {
+                    runBlocking {
+                        PvAPI.authenticate(true)
+                        if (PvAPI.isAuthenticated()) {
+                            SystemToast.add(
+                                McClient.toasts,
+                                SystemToast.SystemToastId.WORLD_BACKUP,
+                                Text.of("Cache Bypassed"),
+                                null,
+                            )
+                        } else {
+                            SystemToast.add(
+                                McClient.toasts,
+                                SystemToast.SystemToastId.WORLD_BACKUP,
+                                Text.of("Failed to bypass cache"),
+                                null,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
