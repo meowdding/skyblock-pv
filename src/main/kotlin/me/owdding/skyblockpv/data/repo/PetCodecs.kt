3dcfaca006d83674b51da532971d1e0fabfc949f
mona <59416038+meowora@users.noreply.github.com>
2025-05-02 22:03:33 +0200

    refactor: move pet data

--- src/main/kotlin/me/owdding/skyblockpv/data/repo/PetCodecs.kt
+++ src/main/kotlin/me/owdding/skyblockpv/data/repo/PetCodecs.kt
@@ -1,48 +1,31 @@
 package me.owdding.skyblockpv.data.repo
 
-import com.google.gson.JsonObject
-import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
-import com.teamresourceful.resourcefullib.common.codecs.CodecExtras
+import me.owdding.ktcodecs.FieldName
+import me.owdding.ktcodecs.GenerateCodec
+import me.owdding.ktcodecs.NamedCodec
+import me.owdding.ktmodules.Module
 import me.owdding.skyblockpv.utils.Utils
-import me.owdding.skyblockpv.utils.codecs.CodecUtils
 import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
-import tech.thatgravyboat.skyblockapi.utils.json.Json.toData
-import kotlin.jvm.optionals.getOrNull
 
+@Module
 object PetCodecs {
     private val rarityOffsets: MutableList<Int> = mutableListOf()
     private val xpCurve: MutableList<Int> = mutableListOf()
     private val overwrites: MutableMap<String, Data> = mutableMapOf()
-    private val defaultData = Data(xpCurve, rarityOffsets, 100)
+    private val defaultData = Data()
 
-    private val DATA_CODEC = RecordCodecBuilder.create {
-        it.group(
-            CodecUtils.CUMULATIVE_INT_LIST_ALT.optionalFieldOf("xp_curve").forGetter(CodecExtras.optionalFor(Data::xpCurve)),
-            Codec.INT.listOf().optionalFieldOf("rarity_offsets").forGetter(CodecExtras.optionalFor(Data::rarityOffsets)),
-            Codec.INT.optionalFieldOf("level_cap", 100).forGetter(Data::levelCap)
-        ).apply(it) { xpCurve, rarityOffsets, levelCap ->
-            Data(xpCurve.getOrNull() ?: this.xpCurve, rarityOffsets.getOrNull() ?: this.rarityOffsets, levelCap)
-        }
-    }
-
-    private val CODEC = RecordCodecBuilder.create {
-        it.group(
-            Codec.INT.listOf().fieldOf("rarity_offsets").forGetter { rarityOffsets },
-            CodecUtils.CUMULATIVE_INT_LIST_ALT.fieldOf("xp_curve").forGetter { xpCurve },
-            Codec.unboundedMap(Codec.STRING, DATA_CODEC).fieldOf("overwrites").forGetter { overwrites}
-        ).apply(it) { rarityOffsets, xpCurve, overwrites ->
-            this.rarityOffsets.addAll(rarityOffsets)
-            this.xpCurve.addAll(xpCurve)
-            this.overwrites.putAll(overwrites.mapKeys { it.key.uppercase() })
-        }
-    }
+    @GenerateCodec
+    data class PetData(
+        @FieldName("rarity_offsets") val rarityOffsets: List<Int>,
+        @NamedCodec("cum_int_list_alt") @FieldName("xp_curve") val xpCurve: List<Int>,
+        val overwrites: Map<String, Data>,
+    )
 
     init {
-        Utils.loadFromRepo<JsonObject>("pets").toData(CODEC).let {
-            if (it == null) {
-                throw IllegalStateException("Failed to load pet data!")
-            }
+        Utils.loadRepoData<PetData>("pets").let {
+            this.rarityOffsets.addAll(it.rarityOffsets)
+            this.xpCurve.addAll(it.xpCurve)
+            this.overwrites.putAll(it.overwrites.mapKeys { (key, _) -> key.uppercase() })
         }
     }
 
@@ -50,7 +33,12 @@ object PetCodecs {
         return overwrites[pet] ?: defaultData
     }
 
-    data class Data(val xpCurve: List<Int>, val rarityOffsets: List<Int>, val levelCap: Int) {
+    @GenerateCodec
+    data class Data(
+        @NamedCodec("cum_int_list_alt") @FieldName("xp_curve") val xpCurve: List<Int> = PetCodecs.xpCurve,
+        @FieldName("rarity_offsets") val rarityOffsets: List<Int> = PetCodecs.rarityOffsets,
+        @FieldName("level_cap") val levelCap: Int = 100,
+    ) {
         fun getOffset(rarity: SkyBlockRarity): Int {
             val ordinal = rarity.ordinal.coerceIn(0, rarityOffsets.size - 1)
             return rarityOffsets[ordinal]
@@ -60,7 +48,4 @@ object PetCodecs {
             return xpCurve.drop(getOffset(rarity)).take(levelCap - 1)
         }
     }
-
-
-
 }
