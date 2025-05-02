e7d44dc1259fbb58ed8d7e620fe7564a91bdb184
mona <59416038+meowora@users.noreply.github.com>
2025-05-02 20:53:44 +0200

    refactor: move rift data

--- src/main/kotlin/me/owdding/skyblockpv/data/repo/RiftCodecs.kt
+++ src/main/kotlin/me/owdding/skyblockpv/data/repo/RiftCodecs.kt
@@ -1,43 +1,27 @@
 package me.owdding.skyblockpv.data.repo
 
-import com.google.gson.JsonObject
-import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
+import me.owdding.ktcodecs.GenerateCodec
 import me.owdding.ktmodules.Module
 import me.owdding.skyblockpv.utils.Utils
 import tech.thatgravyboat.skyblockapi.api.remote.RepoItemsAPI
-import tech.thatgravyboat.skyblockapi.utils.json.Json.toData
 
 @Module
 object RiftCodecs {
     var data: RiftRepoData
         private set
 
-    private val trophyCodec = RecordCodecBuilder.create {
-        it.group(
-            Codec.STRING.fieldOf("id").forGetter(TrophyRepo::id),
-            Codec.STRING.fieldOf("name").forGetter(TrophyRepo::name),
-        ).apply(it, ::TrophyRepo)
-    }
-
     init {
-        val CODEC = RecordCodecBuilder.create {
-            it.group(
-                trophyCodec.listOf().fieldOf("trophies").forGetter(RiftRepoData::trophies),
-                Codec.STRING.listOf().fieldOf("montezuma").forGetter(RiftRepoData::montezuma),
-                Codec.STRING.listOf().fieldOf("eyes").forGetter(RiftRepoData::eyes),
-            ).apply(it, ::RiftRepoData)
-        }
-
-        data = Utils.loadFromRepo<JsonObject>("rift").toData(CODEC) ?: throw IllegalStateException("Failed to load rift data!")
+        data = Utils.loadRepoData<RiftRepoData>("rift")
     }
 
+    @GenerateCodec
     data class RiftRepoData(
         val trophies: List<TrophyRepo>,
         val montezuma: List<String>,
         val eyes: List<String>,
     )
 
+    @GenerateCodec
     data class TrophyRepo(
         val id: String,
         val name: String,
