ba6e6fc8bb6c3b343e0b6b71d67da6c25f9f97df
mona <59416038+meowora@users.noreply.github.com>
2025-05-03 14:01:25 +0200

    refactor: move museum data

--- src/main/kotlin/me/owdding/skyblockpv/data/museum/RepoMuseumData.kt
+++ src/main/kotlin/me/owdding/skyblockpv/data/museum/RepoMuseumData.kt
@@ -1,11 +1,9 @@
 package me.owdding.skyblockpv.data.museum
 
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
-import com.mojang.serialization.Codec
-import com.mojang.serialization.JsonOps
-import com.mojang.serialization.codecs.RecordCodecBuilder
+import me.owdding.ktcodecs.GenerateCodec
+import me.owdding.ktcodecs.NamedCodec
 import me.owdding.skyblockpv.utils.Utils
+import me.owdding.skyblockpv.utils.codecs.CodecUtils
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.skyblockapi.api.data.SkyBlockCategory
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
@@ -14,50 +12,32 @@ import tech.thatgravyboat.skyblockapi.api.remote.RepoItemsAPI
 
 object RepoMuseumData {
 
-    var armor: List<MuseumArmor> = emptyList()
-    var weapons: List<MuseumItem> = emptyList()
-    var rarities: List<MuseumItem> = emptyList()
-    var special: List<String> = emptyList()
+    val armor: List<MuseumArmor>
+    val weapons: List<MuseumItem>
+    val rarities: List<MuseumItem>
+    val special: List<String>
 
-    val museumCategories: MutableList<RepoMuseumCategory> = mutableListOf()
+    val museumCategories: List<RepoMuseumCategory>
     val museumCategoryMap: MutableMap<RepoMuseumCategory, List<MuseumItem>> = mutableMapOf()
 
-    private val CODEC = RecordCodecBuilder.create {
-        it.group(
-            MUSEUM_ARMOR_CODEC.listOf().fieldOf("armor").forGetter { armor },
-            MUSEUM_ITEM_CODEC.listOf().fieldOf("weapons").forGetter { weapons },
-            MUSEUM_ITEM_CODEC.listOf().fieldOf("rarities").forGetter { rarities },
-            Codec.STRING.listOf().fieldOf("special").forGetter { special },
-        ).apply(it) { armor, weapons, rarities, special ->
-            RepoMuseumData.armor = armor.sortedWith(
-                Comparator.comparingInt<MuseumArmor>(
-                    {
-                        it.armorIds.map { RepoItemsAPI.getItem(it) }.maxOf { it.getData(DataTypes.RARITY)?.ordinal ?: 0 }
-                    },
-                ).then(Comparator.comparing { it.id }),
-            )
-            //.sortedBy { ItemAPI.getItem(it.armorIds.first()).getData(DataTypes.RARITY)?.ordinal ?: 0 }
-            RepoMuseumData.weapons = weapons
-            RepoMuseumData.rarities = rarities
-            RepoMuseumData.special = special
-        }
-    }
+    @GenerateCodec
+    @NamedCodec("MuseumData")
+    data class Data(
+        val armor: List<MuseumArmor>,
+        @NamedCodec("museum§item") val weapons: List<MuseumItem>,
+        @NamedCodec("museum§item") val rarities: List<MuseumItem>,
+        val special: List<String>,
+    )
 
     init {
-        val museumData = Utils.loadFromRepo<JsonObject>("museum_data") ?: JsonObject()
-
-        CODEC.parse(JsonOps.INSTANCE, museumData).let {
-            if (it.isError) {
-                throw RuntimeException(it.error().get().message())
-            }
+        Utils.loadRepoData<Data>("museum_data").let {
+            this.armor = it.armor
+            this.weapons = it.weapons
+            this.rarities = it.rarities
+            this.special = it.special
         }
 
-        val museumCategories = Utils.loadFromRepo<JsonArray>("museum_categories") ?: JsonArray()
-        MUSEUM_CATEGORY_CODEC.listOf().parse(JsonOps.INSTANCE, museumCategories).ifSuccess {
-            this.museumCategories.addAll(it)
-        }.ifError {
-            throw RuntimeException(it.error().get().message())
-        }
+        this.museumCategories = Utils.loadRepoData("museum_categories", CodecUtils.list())
 
         evaluateItemCategoryMap()
     }
