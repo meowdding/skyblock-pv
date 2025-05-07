a07bc7229f927ab8046e989c6e196fdc7cb95702
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-07 18:01:45 +0200

    refactor: stuff in PlayerDbAPI Co-authored-by: ThatGravyBoat <gravy@thatgravyboat.tech>
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyblockpv/api/PlayerDbAPI.kt
+++ src/main/kotlin/me/owdding/skyblockpv/api/PlayerDbAPI.kt
@@ -6,9 +6,12 @@ import com.mojang.authlib.GameProfile
 import com.mojang.authlib.properties.PropertyMap
 import com.mojang.authlib.properties.PropertyMap.Serializer
 import kotlinx.coroutines.runBlocking
+import me.owdding.skyblockpv.utils.Utils.toDashlessString
+import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.utils.extentions.asUUID
 import tech.thatgravyboat.skyblockapi.utils.http.Http
 import java.util.*
+import java.util.concurrent.CompletableFuture
 
 private const val API_URL = "https://playerdb.co/api/player/minecraft/%s"
 private val identifier = String(Base64.getDecoder().decode("Y29udGFjdEB0aGF0Z3Jhdnlib2F0LnRlY2g="))
@@ -17,9 +20,21 @@ object PlayerDbAPI {
 
     private val cache = mutableMapOf<String, GameProfile?>()
 
-    fun getUUID(username: String) = cache.getOrPut(username) {
+    init {
+        val user = McClient.self.user
+        val profile = McClient.self.gameProfile
+        cache.put(user.name, profile)
+        cache.put(profile.id.toDashlessString(), profile)
+        cache.put(profile.id.toString(), profile)
+    }
+
+    fun getProfileAsync(usernameOrId: String): CompletableFuture<GameProfile?> = CompletableFuture.supplyAsync {
+        getProfile(usernameOrId)
+    }
+
+    fun getProfile(usernameOrId: String) = cache.getOrPut(usernameOrId) {
         runBlocking {
-            val player = get(username)?.get("data")?.asJsonObject?.get("player")?.asJsonObject
+            val player = get(usernameOrId)?.get("data")?.asJsonObject?.get("player")?.asJsonObject
             val uuid = player?.get("id")?.asUUID()
             val name = player?.get("username")?.asString
 
@@ -27,7 +42,7 @@ object PlayerDbAPI {
                 val profile = GameProfile(uuid, name)
 
                 val gson = GsonBuilder().registerTypeAdapter(PropertyMap::class.java, Serializer()).create()
-                val property = gson.fromJson<PropertyMap?>(player.get("properties"), PropertyMap::class.java)
+                val property = gson.fromJson(player.get("properties"), PropertyMap::class.java)
                 profile.properties.putAll(property)
 
                 profile
