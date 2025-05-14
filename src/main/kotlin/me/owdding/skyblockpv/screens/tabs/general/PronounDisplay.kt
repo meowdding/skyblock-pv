dd641994fef98a789af72179a8b72e99b3da63fe
Gravy Boat <gravy@thatgravyboat.tech>
2025-05-14 02:39:29 -0230

    fix: pronoun tooltip being displays when hovering profiles

--- src/main/kotlin/me/owdding/skyblockpv/screens/tabs/general/PronounDisplay.kt
+++ src/main/kotlin/me/owdding/skyblockpv/screens/tabs/general/PronounDisplay.kt
@@ -3,7 +3,6 @@ package me.owdding.skyblockpv.screens.tabs.general
 import me.owdding.lib.displays.Display
 import me.owdding.lib.displays.Displays
 import me.owdding.lib.displays.centerIn
-import me.owdding.lib.displays.withTooltip
 import me.owdding.skyblockpv.api.pronouns.PronounsDbAPI
 import me.owdding.skyblockpv.utils.displays.ExtraDisplays
 import tech.thatgravyboat.skyblockapi.helpers.McFont
@@ -15,7 +14,7 @@ object PronounDisplay {
             PronounsDbAPI.getPronounsAsync(uuid),
             { (_, pronouns) ->
                 val pronouns = pronouns.firstOrNull() ?: return@completableDisplay Displays.empty()
-                Displays.text("Pronouns: ${pronouns.toDisplay()}", color = { 0x555555u }, shadow = false).centerIn(width, McFont.height).withTooltip("Provided by https://pronoundb.org/")
+                Displays.text("Pronouns: ${pronouns.toDisplay()}", color = { 0x555555u }, shadow = false).centerIn(width, McFont.height)
             },
             { Displays.empty(height = McFont.height, width = width) },
             { Displays.empty(height = McFont.height, width = width) },
