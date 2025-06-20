0da96f876ed3367c210b1f0e303c4c23b8e425cf
Mona <59416038+meowora@users.noreply.github.com>
2025-06-20 17:01:55 +0200

    remove mouse debug

--- src/main/kotlin/me/owdding/skyblockpv/utils/ExtraWidgetRenderers.kt
+++ src/main/kotlin/me/owdding/skyblockpv/utils/ExtraWidgetRenderers.kt
@@ -2,19 +2,16 @@ package me.owdding.skyblockpv.utils
 
 import earth.terrarium.olympus.client.components.base.renderer.WidgetRenderer
 import me.owdding.lib.displays.Display
-import me.owdding.skyblockpv.SkyBlockPv
 import net.minecraft.client.gui.components.AbstractWidget
-import tech.thatgravyboat.skyblockapi.helpers.McFont
 
 object ExtraWidgetRenderers {
 
     fun <T : AbstractWidget> conditional(ifTrue: WidgetRenderer<T>, ifFalse: WidgetRenderer<T>, condition: () -> Boolean): WidgetRenderer<T> =
         WidgetRenderer<T> { graphics, context, partialTicks ->
-            if (SkyBlockPv.isDevMode) {
-                graphics.drawString(McFont.self, context.mouseX.toString(), 0, 150, -1)
-            }
-
-            if (condition()) ifTrue.render(graphics, context, partialTicks) else ifFalse.render(graphics, context, partialTicks)
+            if (condition())
+                ifTrue.render(graphics, context, partialTicks)
+            else
+                ifFalse.render(graphics, context, partialTicks)
         }
 
     fun <T : AbstractWidget> display(display: Display) = WidgetRenderer<T> { graphics, context, partialTicks ->
