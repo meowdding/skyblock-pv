257a94af330b5b8ecfe2ec5ebc7dedd874a3aca5
Mona <59416038+meowora@users.noreply.github.com>
2025-06-21 19:37:41 +0200

    feat: add themed widget sprites

--- src/main/kotlin/me/owdding/skyblockpv/utils/theme/ThemeSupport.kt
+++ src/main/kotlin/me/owdding/skyblockpv/utils/theme/ThemeSupport.kt
@@ -1,7 +1,9 @@
 package me.owdding.skyblockpv.utils.theme
 
 import me.owdding.skyblockpv.SkyBlockPv.id
+import me.owdding.skyblockpv.accessor.WidgetSpritesAccessor
 import me.owdding.skyblockpv.config.Config
+import net.minecraft.client.gui.components.WidgetSprites
 import net.minecraft.resources.ResourceLocation
 
 object ThemeSupport {
@@ -13,5 +15,14 @@ object ThemeSupport {
 
     fun texture(path: String) = texture(id(path))
     fun texture(path: ResourceLocation) = pvTextures.getOrDefault(path, path)
+
+    fun WidgetSprites.withThemeSupport(): WidgetSprites = also { WidgetSpritesAccessor.withThemeSupport(it) }
+
+    fun ThemedWidgetSprites(
+        enabled: ResourceLocation,
+        disabled: ResourceLocation,
+        enabledFocused: ResourceLocation,
+        disabledFocused: ResourceLocation = disabled,
+    ) = WidgetSprites(enabled, disabled, enabledFocused, disabledFocused).withThemeSupport()
 
 }
