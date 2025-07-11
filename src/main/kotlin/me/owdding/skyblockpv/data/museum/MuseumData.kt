ac2c18cc4d2434195dd2f930e7a9c1dfd6db64cc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-11 10:23:58 +0200

    feat: post pv museum opened event
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyblockpv/data/museum/MuseumData.kt
+++ src/main/kotlin/me/owdding/skyblockpv/data/museum/MuseumData.kt
@@ -5,17 +5,24 @@ import me.owdding.skyblockpv.api.data.SkyBlockProfile
 import me.owdding.skyblockpv.utils.Utils.toDashlessString
 import me.owdding.skyblockpv.utils.getNbt
 import me.owdding.skyblockpv.utils.legacyStack
-import net.minecraft.world.item.ItemStack
+import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
+import tech.thatgravyboat.skyblockapi.api.events.remote.MuseumEntry
+import tech.thatgravyboat.skyblockapi.api.events.remote.SkyBlockPvMuseumOpenedEvent
+import tech.thatgravyboat.skyblockapi.api.events.remote.SkyBlockPvRequired
 import tech.thatgravyboat.skyblockapi.utils.json.getPath
 
 data class MuseumData(val items: List<MuseumEntry>) {
     companion object {
+        @OptIn(SkyBlockPvRequired::class)
         fun fromJson(profile: SkyBlockProfile, members: JsonObject?): MuseumData? {
             val asJsonObject = members?.getPath("${profile.userId.toDashlessString()}.items")
                 ?.let { it as? JsonObject } ?: return MuseumData(emptyList())
             val map = asJsonObject.entrySet().map { it.key to it.value as JsonObject }.mapNotNull {
                 it.second.getPath("items.data")?.getNbt()?.getListOrEmpty("i")?.let { value -> it.first to value }
             }.map { MuseumEntry(it.first, it.second.map { item -> lazy { item.legacyStack() } }) }
+
+            SkyBlockPvMuseumOpenedEvent(map).post(SkyBlockAPI.eventBus)
+
             return MuseumData(map)
         }
     }
@@ -31,8 +38,3 @@ data class MuseumData(val items: List<MuseumEntry>) {
         return null
     }
 }
-
-data class MuseumEntry(
-    val id: String,
-    val stacks: List<Lazy<ItemStack>>,
-)
