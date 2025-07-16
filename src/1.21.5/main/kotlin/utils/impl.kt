18769fd7e2a1a1acc01b66fa3c5810acdc707b54
Mona <59416038+meowora@users.noreply.github.com>
2025-07-16 12:09:49 +0200

    i hate 1215 rendering

--- src/1.21.5/main/kotlin/utils/impl.kt
+++ src/1.21.5/main/kotlin/utils/impl.kt
@@ -2,17 +2,16 @@ package me.owdding.skyblockpv.utils
 
 import earth.terrarium.olympus.client.pipelines.RoundedRectangle
 import net.minecraft.client.gui.GuiGraphics
-import tech.thatgravyboat.skyblockapi.platform.pushPop
 
 actual fun GuiGraphics.drawRoundedRec(
     x: Int, y: Int, width: Int, height: Int,
     backgroundColor: Int, borderColor: Int,
     borderSize: Int, radius: Int,
 ) {
-    pushPop {
+    this.flush()
+
         RoundedRectangle.drawRelative(
             this@drawRoundedRec, x, y, width, height,
             backgroundColor, borderColor, width.coerceAtMost(height) * (radius / 100f), borderSize,
         )
-    }
 }
