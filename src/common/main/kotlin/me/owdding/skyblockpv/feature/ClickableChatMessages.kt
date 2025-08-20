3b2ff8af40f9768707e9c6099ca7629b26bf9440
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-20 17:00:07 +0200

    feat: click to open pv in other chats
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyblockpv/feature/ClickableChatMessages.kt
+++ src/common/main/kotlin/me/owdding/skyblockpv/feature/ClickableChatMessages.kt
@@ -8,15 +8,18 @@ import net.minecraft.network.chat.Component
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.match
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.command
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.hover
 
 @Module
 object ClickableChatMessages {
 
+    private val otherChatRegex = "(?:Party|Guild|Officer) > (?:\\[.*] )?(?<username>\\w{1,16})(?: \\[.*])?: .+".toRegex()
+
     @Subscription
     @OnlyOnSkyBlock
-    fun onChat(event: ChatReceivedEvent.Post) {
+    fun onAllChat(event: ChatReceivedEvent.Post) {
         if (!Config.profileChatClick) return
 
         var output = Component.empty()
@@ -32,7 +35,7 @@ object ClickableChatMessages {
             val username = command.removePrefix("/socialoptions ")
             val nameComponent = component.copy()
             nameComponent.command = "/sbpv pv $username"
-            nameComponent.hover = "messages.social_options_hover".asTranslated(username)
+            nameComponent.hover = "messages.click_to_open_pv".asTranslated(username)
 
             output = output.append(nameComponent)
             hasReplaced = true
@@ -40,5 +43,18 @@ object ClickableChatMessages {
 
         if (!hasReplaced) return
         event.component = output
+    }
+
+    @Subscription
+    @OnlyOnSkyBlock
+    fun onOtherChat(event: ChatReceivedEvent.Post) {
+        if (!Config.profileChatClickOther) return
+
+        otherChatRegex.match(event.text, "username") { (username) ->
+            event.component = event.component.copy().apply {
+                command = "/sbpv pv $username"
+                hover = "messages.click_to_open_pv".asTranslated(username)
+            }
+        }
     }
 }
