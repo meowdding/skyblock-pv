070ccb992dac1a5d5f3459288b4bdbe3d221cd47
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-16 12:09:27 +0200

    fix: unnecessary McVersion
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyblockpv/config/Config.kt
+++ src/common/main/kotlin/me/owdding/skyblockpv/config/Config.kt
@@ -3,7 +3,6 @@ package me.owdding.skyblockpv.config
 import com.teamresourceful.resourcefulconfig.api.types.info.ResourcefulConfigLink
 import com.teamresourceful.resourcefulconfig.api.types.options.TranslatableValue
 import com.teamresourceful.resourcefulconfigkt.api.ConfigKt
-import me.owdding.skyblockpv.McVersion
 import me.owdding.skyblockpv.SkyBlockPv
 import me.owdding.skyblockpv.SkyBlockPv.id
 import me.owdding.skyblockpv.utils.Utils
@@ -45,14 +44,7 @@ object Config : ConfigKt("skyblockpv/config") {
     var partyFinderMessage by boolean(true) { this.translation = "skyblockpv.config.party_finder_message" }
     var disableOutsideHypixel by boolean(false) { this.translation = "skyblockpv.config.disable_outside_hypixel" }
     var skillOverflow by boolean(false) { this.translation = "skyblockpv.config.skill_overflow" }
-    var displayScaling by boolean(false) scaling@{
-        if (McVersion.MC_1_21_8.isActive()) {
-            this.translation = "skyblockpv.config.display_scaling.1218"
-            return@scaling
-        }
-
-        this.translation = "skyblockpv.config.display_scaling"
-    }
+    var displayScaling by boolean(false) { this.translation = "skyblockpv.config.display_scaling" }
     var theme by transform(
         string("skyblock-pv:default") {
             renderer = THEME_RENDERER
