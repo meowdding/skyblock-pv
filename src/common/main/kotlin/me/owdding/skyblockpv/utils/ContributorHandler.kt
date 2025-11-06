cae6dc078e2d6d135b66543dcd742f5291551604
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-06 15:36:42 +0100

    move ContributorData.EMPTY
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyblockpv/utils/ContributorHandler.kt
+++ src/common/main/kotlin/me/owdding/skyblockpv/utils/ContributorHandler.kt
@@ -1,8 +1,5 @@
 package me.owdding.skyblockpv.utils
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import me.owdding.ktcodecs.FieldName
 import me.owdding.ktcodecs.GenerateCodec
 import me.owdding.ktcodecs.NamedCodec
@@ -17,23 +14,19 @@ import net.minecraft.network.chat.TextColor
 import net.minecraft.world.entity.animal.Parrot
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.utils.json.Json.toData
-import java.net.URI
 import java.util.*
-import kotlin.collections.contains
 
 @Module
 object ContributorHandler {
     var contributors: MutableMap<UUID, ContributorData> = mutableMapOf()
         private set
 
-    val emptyData = ContributorData(null, null, null, false, null)
-
     @Subscription
     fun onCostmeticLoad(event: CosmeticLoadEvent) {
         val contributorData = CosmeticManager.playerList.associateNotNull(
             keySelector = { it.uuid },
             valueSelector = { playerEntry ->
-                playerEntry.data.toData(SkyBlockPVCodecs.getCodec<ContributorData>()).takeUnless { data -> data == emptyData }
+                playerEntry.data.toData(SkyBlockPVCodecs.getCodec<ContributorData>()).takeUnless { data -> data == ContributorData.EMPTY }
             },
         )
         contributors.putAll(contributorData)
@@ -49,6 +42,10 @@ data class ContributorData(
     @FieldName("pv:title_colors") val tileColors: List<TextColor>?,
 ) {
     val titleShader = tileColors?.let { GradientTextShader(*it.toTypedArray()) }
+
+    companion object {
+        val EMPTY = ContributorData(null, null, null, false, null)
+    }
 }
 
 @GenerateCodec
