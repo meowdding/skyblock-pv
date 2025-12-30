c7c4bea5551b1295bda0d5d06295648afb28ba9f
chris-period <160649430+chris-period@users.noreply.github.com>
2025-12-30 12:13:31 -0800

    Configured gradle.properties to speed up build times. (Parallel, Cache, Daemon).
    Modified Config.kt patches so partyFinderMessage is not defaulted "Disabled" an invalid PartyFinderJoin.State.

--- src/main/kotlin/me/owdding/skyblockpv/config/Config.kt
+++ src/main/kotlin/me/owdding/skyblockpv/config/Config.kt
@@ -61,7 +61,12 @@ object Config : ConfigKt("skyblockpv/config") {
 
     override val patches: Map<Int, UnaryOperator<JsonObject>> = mapOf(
         0 to UnaryOperator { json ->
-            json.addProperty("partyFinderMessage", if (json.get("partyFinderMessage")?.asBoolean ?: false) "OPEN_PV" else "DISABLED")
+            val state = json.get("partyFinderMessage")?.asString
+                ?.uppercase()
+                ?.let { PartyFinderJoin.State.valueOf(it) }
+                ?: PartyFinderJoin.State.OFF // grab the STATE or default to OFF
+
+            json.addProperty("partyFinderMessage", state.name)
             json
         },
     )
